227c3ba85692b3f64facf0a317bc8547f8b7bbdd
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-05-28 12:25:36 +0000

    use PAXRUNNER-4 for choosing the runtime platform

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -jar %USERPROFILE%\.m2\repository\org\ops4j\pax\runner\0.3.2-SNAPSHOT\runner-0.3.2-SNAPSHOT.jar org.ops4j.pax.web demo 0.2.0-SNAPSHOT
+java -jar %USERPROFILE%\.m2\repository\org\ops4j\pax\runner\0.3.2-SNAPSHOT\runner-0.3.2-SNAPSHOT.jar --platform=choose org.ops4j.pax.web demo 0.2.0-SNAPSHOT
