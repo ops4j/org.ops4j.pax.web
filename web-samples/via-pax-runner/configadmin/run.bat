23808e094bc29bfaa968228eee7b5c6e215f4b39
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-07-03 14:00:57 +0000

    PAXWEB-30 Move up.

--- web-samples/via-pax-runner/configadmin/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-if "%1"=="debug" set _VM_=%_VM_% -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
-java -jar %USERPROFILE%\.m2\repository\org\ops4j\pax\runner\0.3.2\runner-0.3.2.jar --platform=choose org.ops4j.pax.web.samples.via-pax-runner %_VM_% configadmin 0.1.0-SNAPSHOT
--- web-samples/via-pax-runner/configadmin/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -jar $HOME/.m2/repository/org/ops4j/pax/runner/0.3.2/runner-0.3.2.jar --platform=choose org.ops4j.pax.web.samples.via-pax-runner configadmin 0.1.0-SNAPSHOT
