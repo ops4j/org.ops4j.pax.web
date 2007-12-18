7c931d7b5a4e2f693e040692e146588a1ab554c6
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-12-18 22:53:59 +0000

    Move web samples under web directory. Soon it will be followed by a removng of unnecessary complexity.

---
+++ web-samples/authentication/auth.simple/src/main/java/org/ops4j/pax/web/samples/auth/simple/internal/Activator.java
@@ -0,0 +1,49 @@
+package org.ops4j.pax.web.samples.auth.simple.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Extension of the default OSGi bundle activator
+ */
+public final class Activator
+    implements BundleActivator
+{
+
+    private ServiceReference m_httpServiceRef;
+    private HttpService m_httpService;
+
+    /**
+     * Called whenever the OSGi framework starts our bundle
+     */
+    public void start( BundleContext bc )
+        throws Exception
+    {
+        m_httpServiceRef = bc.getServiceReference( HttpService.class.getName() );
+        if ( m_httpServiceRef != null )
+        {
+            m_httpService = (HttpService) bc.getService( m_httpServiceRef );
+            m_httpService.registerServlet("/status", new StatusServlet(), null, null);
+            m_httpService.registerServlet("/status-with-auth", new StatusServlet(), null, new AuthHttpContext());
+        }
+    }
+
+    /**
+     * Called whenever the OSGi framework stops our bundle
+     */
+    public void stop( BundleContext bc )
+        throws Exception
+    {
+        if ( m_httpService != null )
+        {
+            bc.ungetService( m_httpServiceRef );
+            m_httpServiceRef = null;
+            m_httpService = null;
+        }        
+    }
+}
+
