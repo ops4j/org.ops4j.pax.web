e2541da92700c7c5a8410c6d451220b0bad9f765
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-12-21 16:17:35 +0000

    Send back to the client HttpServletResponse.SC_UNAUTHORIZED in case that HttpContext returns false on handleSecurity. Also, if the HttpContext does not set the status, set teh status to HttpServletResponse.SC_UNAUTHORIZED (401).

---
+++ service/src/main/java/org/ops4j/pax/web/service/internal/HttpServiceResponseWrapper.java
@@ -0,0 +1,79 @@
+/*  Copyright 2007 Alin Dreghiciu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.web.service.internal;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * An HttpServletResponseWrapper that exposes the status of response.
+ *
+ * @author Alin Dreghiciu
+ * @since 0.2.3, December 21, 2007
+ */
+public class HttpServiceResponseWrapper extends HttpServletResponseWrapper
+{
+
+    /**
+     * Logger.
+     */
+    private static final Log LOG = LogFactory.getLog( HttpServiceResponseWrapper.class );
+    /**
+     * Response status.
+     */
+    private int m_status;
+
+    /**
+     * Constructs a response adaptor wrapping the given response.
+     *
+     * @throws IllegalArgumentException if the response is null
+     */
+    public HttpServiceResponseWrapper( final HttpServletResponse response )
+    {
+        super( response );
+    }
+
+    @Override
+    public void setStatus( int sc )
+    {
+        super.setStatus( sc );
+        m_status = sc;
+    }
+
+    /**
+     * Returns the status if it had been set, or zero if not set.
+     *
+     * @return status
+     */
+    public int getStatus()
+    {
+        return m_status;
+    }
+
+    /**
+     * Returns true if status has been set.
+     *
+     * @return true if status has been set.
+     */
+    public boolean isStatusSet()
+    {
+        System.out.println( "Status: " + m_status );  
+        return m_status != 0;
+    }
+}
