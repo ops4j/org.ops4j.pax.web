ae14b1db2c13287a32abdc8388e2a21b193748e2
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-01-02 13:46:22 +0000

    Add a basic example (hellow world) for http service.

---
+++ samples/helloworld-hs/src/main/java/org/ops4j/pax/web/samples/helloworld/hs/internal/Activator.java
@@ -0,0 +1,66 @@
+package org.ops4j.pax.web.samples.helloworld.hs.internal;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpContext;
+import org.osgi.service.http.HttpService;
+
+/**
+ * Extension of the default OSGi bundle activator
+ */
+public final class Activator
+    implements BundleActivator
+{
+
+    private ServiceReference m_httpServiceRef;
+
+    /**
+     * Called when the OSGi framework starts our bundle
+     */
+    public void start( BundleContext bc )
+        throws Exception
+    {
+        m_httpServiceRef = bc.getServiceReference( HttpService.class.getName() );
+        if( m_httpServiceRef != null )
+        {
+            final HttpService httpService = (HttpService) bc.getService( m_httpServiceRef );
+            if( httpService != null )
+            {
+                // create a default context to share between registrations
+                final HttpContext httpContext = httpService.createDefaultHttpContext();
+                // register the hello world servlet
+                final Dictionary initParams = new Hashtable();
+                initParams.put( "from", "HttpService" );
+                httpService.registerServlet(
+                    "/helloworld",              // alias
+                    new HelloWorldServlet(),    // registered servlet
+                    initParams,                 // init params
+                    httpContext                 // http context
+                );
+                // register images as resources
+                httpService.registerResources(
+                    "/images",
+                    "/images",
+                    httpContext
+                );
+            }
+        }
+    }
+
+    /**
+     * Called when the OSGi framework stops our bundle
+     */
+    public void stop( BundleContext bc )
+        throws Exception
+    {
+        if( m_httpServiceRef != null )
+        {
+            bc.ungetService( m_httpServiceRef );
+            m_httpServiceRef = null;
+        }
+    }
+}
+
---
+++ samples/helloworld-hs/src/main/java/org/ops4j/pax/web/samples/helloworld/hs/internal/HelloWorldServlet.java
@@ -0,0 +1,28 @@
+package org.ops4j.pax.web.samples.helloworld.hs.internal;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet
+    extends HttpServlet
+{
+
+    protected void doGet( final HttpServletRequest request, final HttpServletResponse response )
+        throws ServletException, IOException
+    {
+        response.setContentType( "text/html" );
+
+        final PrintWriter writer = response.getWriter();
+        writer.println( "<html><body align='center'>" );
+        writer.println( "<h1>Hello World</h1>" );
+        writer.println( "<img src='/images/logo.png' border='0'/>" );
+        writer.println( "<h1>" + getServletConfig().getInitParameter( "from" ) + "</h1>" );
+        writer.println( "</body></html>" );
+    }
+
+}
+
