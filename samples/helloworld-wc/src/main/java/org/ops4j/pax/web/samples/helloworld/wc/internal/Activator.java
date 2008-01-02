650dfe33e913f70f20e5e5f5d8459e711e8174e4
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-01-02 14:18:07 +0000

    Add copyright info and JavaDoc.

--- samples/helloworld-wc/src/main/java/org/ops4j/pax/web/samples/helloworld/wc/internal/Activator.java
+++ samples/helloworld-wc/src/main/java/org/ops4j/pax/web/samples/helloworld/wc/internal/Activator.java
@@ -1,3 +1,19 @@
+/* Copyright 2008 Alin Dreghiciu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ops4j.pax.web.samples.helloworld.wc.internal;
 
 import java.util.Dictionary;
@@ -9,12 +25,18 @@ import org.osgi.service.http.HttpContext;
 import org.ops4j.pax.web.service.WebContainer;
 
 /**
- * Extension of the default OSGi bundle activator
+ * Hello World Activator.
+ *
+ * @author Alin Dreghiciu
+ * @since 0.2.4, January 02, 2007
  */
 public final class Activator
     implements BundleActivator
 {
 
+    /**
+     * WebContainer reference.
+     */
     private ServiceReference m_webContainerRef;
 
     /**
--- samples/helloworld-wc/src/main/java/org/ops4j/pax/web/samples/helloworld/wc/internal/HelloWorldServlet.java
+++ samples/helloworld-wc/src/main/java/org/ops4j/pax/web/samples/helloworld/wc/internal/HelloWorldServlet.java
@@ -1,3 +1,19 @@
+/* Copyright 2008 Alin Dreghiciu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ops4j.pax.web.samples.helloworld.wc.internal;
 
 import java.io.IOException;
@@ -7,6 +23,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Hello World Servlet.
+ *
+ * @author Alin Dreghiciu
+ * @since 0.2.4, January 02, 2008
+ */
 public class HelloWorldServlet
     extends HttpServlet
 {
