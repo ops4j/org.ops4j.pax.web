67f63694889b405989fea0068c0fcb12e3c5da41
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-01-10 12:01:46 +0000

    Finish jasper implementation.

--- jsp/src/main/java/org/ops4j/pax/web/jsp/JspServletWrapper.java
+++ jsp/src/main/java/org/ops4j/pax/web/jsp/JspServletWrapper.java
@@ -16,7 +16,19 @@
  */
 package org.ops4j.pax.web.jsp;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.jasper.servlet.JspServlet;
+import org.osgi.framework.Bundle;
+import org.ops4j.pax.swissbox.lang.ContextClassLoaderUtils;
+import org.ops4j.pax.web.jsp.internal.JasperClassLoader;
 
 /**
  * Wrapper of Jasper JspServlet that knows how to deal with resources loaded from osgi context.
@@ -25,7 +37,171 @@ import javax.servlet.http.HttpServlet;
  * @since 0.3.0, January 07, 2008
  */
 public class JspServletWrapper
-    extends HttpServlet
+    implements Servlet
 {
 
+    /**
+     * Logger.
+     */
+    private static final Log LOG = LogFactory.getLog( JspServletWrapper.class );
+    /**
+     * Jasper Servlet.
+     */
+    private final JspServlet m_jasperServlet;
+    /**
+     * Jasper specific class loader.
+     */
+    private final JasperClassLoader m_jasperClassLoader;
+
+    public JspServletWrapper( final Bundle bundle )
+    {
+        m_jasperServlet = new JspServlet();
+        m_jasperClassLoader = new JasperClassLoader( bundle, JasperClassLoader.class.getClassLoader() );
+        LOG.debug( "Class loader [" + m_jasperClassLoader + "]" );
+    }
+
+    /**
+     * Delegates to jasper servlet with a controlled context class loader.
+     *
+     * @see JspServlet#init(ServletConfig)
+     */
+    public void init( final ServletConfig config )
+        throws ServletException
+    {
+        try
+        {
+            ContextClassLoaderUtils.doWithClassLoader(
+                m_jasperClassLoader,
+                new Callable<Void>()
+                {
+
+                    public Void call()
+                        throws Exception
+                    {
+                        m_jasperServlet.init( config );
+                        return null;
+                    }
+
+                }
+            );
+        }
+        catch( ServletException e )
+        {
+            // re-thrown
+            throw e;
+        }
+        catch( RuntimeException e )
+        {
+            // re-thrown
+            throw e;
+        }
+        catch( Exception ignore )
+        {
+            // ignored as it should never happen
+            LOG.error( "Ignored exception", ignore );
+        }
+    }
+
+    /**
+     * Delegates to jasper servlet.
+     *
+     * @see JspServlet#getServletConfig()
+     */
+    public ServletConfig getServletConfig()
+    {
+        return m_jasperServlet.getServletConfig();
+    }
+
+    /**
+     * Delegates to jasper servlet with a controlled context class loader.
+     *
+     * @see JspServlet#service(ServletRequest, ServletResponse)
+     */
+    public void service( final ServletRequest req, final ServletResponse res )
+        throws ServletException, IOException
+    {
+        try
+        {
+            ContextClassLoaderUtils.doWithClassLoader(
+                m_jasperClassLoader,
+                new Callable<Void>()
+                {
+
+                    public Void call()
+                        throws Exception
+                    {
+                        m_jasperServlet.service( req, res );
+                        return null;
+                    }
+
+                }
+            );
+        }
+        catch( ServletException e )
+        {
+            // re-thrown
+            throw e;
+        }
+        catch( IOException e )
+        {
+            // re-thrown
+            throw e;
+        }
+        catch( RuntimeException e )
+        {
+            // re-thrown
+            throw e;
+        }
+        catch( Exception ignore )
+        {
+            // ignored as it should never happen
+            LOG.error( "Ignored exception", ignore );
+        }
+    }
+
+    /**
+     * Delegates to jasper servlet.
+     *
+     * @see JspServlet#getServletInfo()
+     */
+    public String getServletInfo()
+    {
+        return m_jasperServlet.getServletInfo();
+    }
+
+    /**
+     * Delegates to jasper servlet with a controlled context class loader.
+     *
+     * @see JspServlet#destroy()
+     */
+    public void destroy()
+    {
+        try
+        {
+            ContextClassLoaderUtils.doWithClassLoader(
+                m_jasperClassLoader,
+                new Callable<Void>()
+                {
+
+                    public Void call()
+                        throws Exception
+                    {
+                        m_jasperServlet.destroy();
+                        return null;
+                    }
+
+                }
+            );
+        }
+        catch( RuntimeException e )
+        {
+            // re-thrown
+            throw e;
+        }
+        catch( Exception ignore )
+        {
+            // ignored as it should never happen
+            LOG.error( "Ignored exception", ignore );
+        }
+    }
 }
