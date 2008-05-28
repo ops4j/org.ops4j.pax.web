8a4f15532c523447168d14ccdc3ffd3a5d5cafba
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-05-28 10:24:56 +0000

    PAXWEB-91 Temporary solution?: Disable authentication in case that the http request is not an Jetty Request and issue a warning log message.

--- bundle/src/main/java/org/ops4j/pax/web/service/internal/HttpServiceRequestWrapper.java
+++ bundle/src/main/java/org/ops4j/pax/web/service/internal/HttpServiceRequestWrapper.java
@@ -26,7 +26,7 @@ import org.osgi.service.http.HttpContext;
 import org.ops4j.lang.NullArgumentException;
 
 /**
- * A http servlet request wrapper that can handle authentication as pecified for http service.
+ * A http servlet request wrapper that can handle authentication as specified for http service.
  *
  * @author Alin Dreghiciu
  * @since December 10, 1007
@@ -44,6 +44,10 @@ class HttpServiceRequestWrapper extends HttpServletRequestWrapper
      * Jetty request.
      */
     private final Request m_request;
+    /**
+     * Original request.
+     */
+    private final HttpServletRequest m_originalRequest;
 
     /**
      * Constructs a request object wrapping the given request .
@@ -55,11 +59,15 @@ class HttpServiceRequestWrapper extends HttpServletRequestWrapper
     HttpServiceRequestWrapper( final HttpServletRequest request )
     {
         super( request );
-        if( !( request instanceof Request ) )
+        m_originalRequest = request;
+        if( request instanceof Request )
         {
-            throw new IllegalArgumentException( "Request must be a jetty specific request" );
+            m_request = (Request) request;
         }
-        m_request = (Request) request;
+        else
+        {
+            m_request = null;
+        }
     }
 
     /**
@@ -90,18 +98,31 @@ class HttpServiceRequestWrapper extends HttpServletRequestWrapper
      */
     private void handleAuthenticationType( final Object authenticationType )
     {
-        if( authenticationType != null )
+        if( m_request != null )
         {
-            // be defensive
-            if( !( authenticationType instanceof String ) )
+            if( authenticationType != null )
             {
-                final String message = "Attribute " + HttpContext.AUTHENTICATION_TYPE
-                                       + " expected to be a String but was an [" + authenticationType.getClass() + "]";
-                LOG.error( message );
-                throw new IllegalArgumentException( message );
+                // be defensive
+                if( !( authenticationType instanceof String ) )
+                {
+                    final String message = "Attribute " + HttpContext.AUTHENTICATION_TYPE
+                                           + " expected to be a String but was an [" + authenticationType.getClass()
+                                           + "]";
+                    LOG.error( message );
+                    throw new IllegalArgumentException( message );
+                }
             }
+            m_request.setAuthType( (String) authenticationType );
         }
-        m_request.setAuthType( (String) authenticationType );
+        else
+        {
+            LOG.warn(
+                "Authentication type cannot be set to " + authenticationType
+                + ". HttpService specific authentication is disabled because the ServletRequest object cannot be used: "
+                + "Expected to be an instance of " + Request.class.getName()
+                + " but got " + m_originalRequest.getClass().getName() + "."
+            );
+        }
     }
 
     /**
@@ -111,20 +132,33 @@ class HttpServiceRequestWrapper extends HttpServletRequestWrapper
      */
     private void handleRemoteUser( final Object remoteUser )
     {
-        Principal userPrincipal = null;
-        if( remoteUser != null )
+        if( m_request != null )
         {
-            // be defensive
-            if( !( remoteUser instanceof String ) )
+            Principal userPrincipal = null;
+            if( remoteUser != null )
             {
-                final String message = "Attribute " + HttpContext.REMOTE_USER + " expected to be a String but was an ["
-                                       + remoteUser.getClass() + "]";
-                LOG.error( message );
-                throw new IllegalArgumentException( message );
+                // be defensive
+                if( !( remoteUser instanceof String ) )
+                {
+                    final String message =
+                        "Attribute " + HttpContext.REMOTE_USER
+                        + " expected to be a String but was an [" + remoteUser.getClass() + "]";
+                    LOG.error( message );
+                    throw new IllegalArgumentException( message );
+                }
+                userPrincipal = new User( (String) remoteUser );
             }
-            userPrincipal = new User( (String) remoteUser );
+            m_request.setUserPrincipal( userPrincipal );
         }
-        m_request.setUserPrincipal( userPrincipal );
+        else
+        {
+            LOG.warn(
+                "Remote user cannot be set to " + remoteUser
+                + ". HttpService specific authentication is disabled because the ServletRequest object cannot be used: "
+                + "Expected to be an instance of " + Request.class.getName()
+                + " but got " + m_originalRequest.getClass().getName() + "."
+            );
+        }
     }
 
     /**
