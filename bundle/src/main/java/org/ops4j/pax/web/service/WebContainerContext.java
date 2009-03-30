53621ed37f4acc11b4be452ca7c6316752921528
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-03-30 19:23:37 +0000

    PAXWEB-129 Implement an OSGi specific ServletContext.getResourcePaths() by delegation to WebContainerContext (new)

---
+++ bundle/src/main/java/org/ops4j/pax/web/service/WebContainerContext.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2009 Alin Dreghiciu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.web.service;
+
+import java.util.Set;
+import org.osgi.service.http.HttpContext;
+
+/**
+ * {@link HttpContext} extensions.
+ *
+ * @author Alin Dreghiciu (adreghiciu@gmail.com)
+ * @since 0.5.3, March 30, 2009
+ */
+public interface WebContainerContext
+    extends HttpContext
+{
+
+    /**
+     * Returns a set of all the paths (String objects) to entries within the web application whose longest sub-path
+     * matches the supplied path argument. A specified path of Ò/Ó indicates the root of the web application.
+     *
+     * @param name the path name for which to return resource paths
+     *
+     * @return a set of the resource paths (String objects) or null if no resource paths could be found or if the caller
+     *         does not have the appropriate permissions.
+     */
+    Set<String> getResourcePaths( String name );
+
+}
