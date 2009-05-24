689ce2ca8fad73d0ebafe0f7c312c2e436e6461d
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-05-24 09:27:10 +0000

    PAXWEB-145 Merge Pax Web Extender into Pax Web

---
+++ samples/war/src/main/java/org/ops4j/pax/web/extender/samples/war/internal/ExampleServlet.java
@@ -0,0 +1,25 @@
+package org.ops4j.pax.web.extender.samples.war.internal;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ExampleServlet
+    extends HttpServlet
+{
+
+    protected void doGet( HttpServletRequest request, HttpServletResponse response )
+        throws ServletException, IOException
+    {
+        final PrintWriter writer = response.getWriter();
+        writer.println( "<body align='center'>" );
+        writer.println( "<h1>Hello World</h1>" );
+        writer.println( "<img src='/images/logo.png' border='0'/>" );
+        writer.println( "<h1>from WEB-INF/classes</h1>" );
+        writer.println( "</body>" );
+    }
+
+}
