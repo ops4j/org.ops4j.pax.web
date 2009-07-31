42ac25fddc5200abb0374b9c25893be3dc05a655
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-07-31 13:41:22 +0000

    [PAXWEB-160] Extract jetty related implementation to a separated bundle

---
+++ pax-web-jetty-bundle/src/main/java/org/ops4j/pax/web/service/jetty/internal/JettyBundleActivator.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2009 Alin Dreghiciu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.web.service.jetty.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * TODO Add JavaDoc.
+ *
+ * @author Alin Dreghiciu (adreghiciu@gmail.com)
+ * @since 0.7.0, July 31, 2009
+ */
+public class JettyBundleActivator
+    implements BundleActivator
+{
+
+    private BundleActivator m_paxWebActivator;
+    private BundleActivator m_jettyActivator;
+
+    public JettyBundleActivator()
+    {
+        m_paxWebActivator = new org.ops4j.pax.web.service.internal.Activator();
+        m_jettyActivator = new org.ops4j.pax.web.service.jetty.internal.Activator();
+    }
+
+    public void start( BundleContext bundleContext )
+        throws Exception
+    {
+        m_jettyActivator.start( bundleContext );
+        m_paxWebActivator.start( bundleContext );
+    }
+
+    public void stop( BundleContext bundleContext )
+        throws Exception
+    {
+        m_paxWebActivator.stop( bundleContext );
+        m_jettyActivator.stop( bundleContext );
+    }
+
+}
