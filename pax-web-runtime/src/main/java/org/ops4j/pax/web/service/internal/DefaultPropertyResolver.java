ee5fe8b84ff82670d4a486a1edd849c25fdf5d03
ANierbeck <bcanhome@googlemail.com>
2011-01-16 21:59:54 +0100

    [PAXWEB-227] - Default configuration should only be configured in one place, configuration is provided by the metatype.xml which can be with metatype service for viewing the configuration for configuration admin service.

--- pax-web-runtime/src/main/java/org/ops4j/pax/web/service/internal/DefaultPropertyResolver.java
+++ pax-web-runtime/src/main/java/org/ops4j/pax/web/service/internal/DefaultPropertyResolver.java
@@ -18,14 +18,25 @@
 package org.ops4j.pax.web.service.internal;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Dictionary;
 import java.util.Hashtable;
+
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import static org.ops4j.pax.web.service.WebContainerConstants.*;
 
+import static org.ops4j.util.xml.ElementHelper.getChild;
+import static org.ops4j.util.xml.ElementHelper.getChildren;
+import static org.ops4j.util.xml.ElementHelper.getRootElement;
+
 import org.ops4j.pax.web.service.internal.util.JspSupportUtils;
 import org.ops4j.util.property.DictionaryPropertyResolver;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 /**
  * Default property resolver.
@@ -44,47 +55,64 @@ class DefaultPropertyResolver
         super( getDefaltProperties() );
     }
 
-    private static Dictionary getDefaltProperties()
+    private static Dictionary<String, String> getDefaltProperties()
     {
         Dictionary<String, String> properties = new Hashtable<String, String>();
-        properties.put( PROPERTY_HTTP_PORT, "8080" );
-        properties.put( PROPERTY_HTTP_USE_NIO, Boolean.TRUE.toString() );
-        properties.put( PROPERTY_HTTP_ENABLED, Boolean.TRUE.toString() );
-        properties.put( PROPERTY_HTTP_SECURE_PORT, "8443" );
-        properties.put( PROPERTY_HTTP_SECURE_ENABLED, Boolean.FALSE.toString() );
-        properties.put( PROPERTY_SSL_KEYSTORE, System.getProperty( "user.home" ) + File.separator + ".keystore" );
-        //properties.put( PROPERTY_SSL_PASSWORD, null );
-        //properties.put( PROPERTY_SSL_KEYPASSWORD, null );
-        // create a temporary directory
-        try
-        {
-            File temporaryDirectory = File.createTempFile( ".paxweb", "" );
-            temporaryDirectory.delete();
-            temporaryDirectory = new File( temporaryDirectory.getAbsolutePath() );
-            temporaryDirectory.mkdirs();
-            temporaryDirectory.deleteOnExit();
-            properties.put( PROPERTY_TEMP_DIR, temporaryDirectory.getCanonicalPath() );
-        }
-        catch( Exception e )
-        {
-            LOG.warn( "Could not create temporary directory. Reason: " + e.getMessage() );
-            //properties.put( PROPERTY_TEMP_DIR, null );
-        }
-        //properties.put( PROPERTY_SESSION_TIMEOUT, null ); // no timeout
         
-        //properties for JSP defaults, this is duplicate since all information is also stored in the metatype.xml 
-        //but needs to be made better in a future version.
-        if (JspSupportUtils.jspSupportAvailable()) { //only works if the JSPs are available. 
-        	properties.put(org.ops4j.pax.web.jsp.JspWebdefaults.PROPERTY_JSP_CHECK_INTERVAL, "300");
-        	properties.put(org.ops4j.pax.web.jsp.JspWebdefaults.PROPERTY_JSP_DEBUG_INFO, Boolean.TRUE.toString());
-        	properties.put(org.ops4j.pax.web.jsp.JspWebdefaults.PROPERTY_JSP_DEVELOPMENT, Boolean.TRUE.toString());
-        	properties.put(org.ops4j.pax.web.jsp.JspWebdefaults.PROPERTY_JSP_ENABLE_POOLING, Boolean.TRUE.toString());
-        	properties.put(org.ops4j.pax.web.jsp.JspWebdefaults.PROPERTY_JSP_IE_CLASS_ID, "clsid:8AD9C840-044E-11D1-B3E9-00805F499D93");
-        	properties.put(org.ops4j.pax.web.jsp.JspWebdefaults.PROPERTY_JSP_JAVA_ENCODING, "UTF-8");
-        	properties.put(org.ops4j.pax.web.jsp.JspWebdefaults.PROPERTY_JSP_KEEP_GENERATED, Boolean.TRUE.toString());
-        	properties.put(org.ops4j.pax.web.jsp.JspWebdefaults.PROPERTY_JSP_LOG_VERBOSITY_LEVEL, "WARNING");
-        	properties.put(org.ops4j.pax.web.jsp.JspWebdefaults.PROPERTY_JSP_TAGPOOL_MAX_SIZE, "5");
-        }
+        InputStream stream = DefaultPropertyResolver.class.getClassLoader().getResourceAsStream("OSGI-INF/metatype/metatype.xml");
+        try {
+			Element rootElement = getRootElement(stream);
+			Element[] children = getChildren(rootElement, "AD");
+			for (Element element : children) {
+				String id = element.getAttribute("id");
+				String required = element.getAttribute("required");
+				String defaultAttribute = element.getAttribute("default");
+				if (Boolean.parseBoolean(required)) {
+					//it's a mandatory field initialize it even with a null
+					properties.put(id, defaultAttribute);
+				} else {
+					if(defaultAttribute != null && defaultAttribute.length() > 0) {
+						//it's no mandatory but it is configured, use it anyway
+						properties.put(id, defaultAttribute);
+					}
+				}
+			}
+			
+		} catch (ParserConfigurationException e) {
+			LOG.error( "Could not parse metatype.xml. Reason: " + e.getMessage() );
+			return properties;
+		} catch (IOException e) {
+			LOG.error( "Could not parse metatype.xml. Reason: " + e.getMessage() );
+			return properties;
+		} catch (SAXException e) {
+			LOG.error( "Could not parse metatype.xml. Reason: " + e.getMessage() );
+			return properties;
+		}
+        
+        // create a temporary directory
+		if (properties.get(PROPERTY_TEMP_DIR) != null) {
+			//check if the provided temp directory exists
+			File temporaryDirectory = new File(properties.get(PROPERTY_TEMP_DIR));
+			if (!temporaryDirectory.exists()) {
+				temporaryDirectory.mkdirs(); //since this is a provided temp directory it is not cleared after shutdown.
+			}
+		} else {
+			//temp directory doesn't exist create it. 
+	        try
+	        {
+	            File temporaryDirectory = File.createTempFile( ".paxweb", "" );
+	            temporaryDirectory.delete();
+	            temporaryDirectory = new File( temporaryDirectory.getAbsolutePath() );
+	            temporaryDirectory.mkdirs();
+	            temporaryDirectory.deleteOnExit();
+	            properties.put( PROPERTY_TEMP_DIR, temporaryDirectory.getCanonicalPath() );
+	        }
+	        catch( Exception e )
+	        {
+	            LOG.warn( "Could not create temporary directory. Reason: " + e.getMessage() );
+	            //properties.put( PROPERTY_TEMP_DIR, null );
+	        }
+		}
         return properties; 
     }
 
