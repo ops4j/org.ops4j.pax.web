dc7ff778af100654f04767275ee8a53f59ae0832
ANierbeck <bcanhome@googlemail.com>
2011-02-15 23:23:52 +0100

    [PAXWEB-242] - Add Integration tests first Integration Test for standard war archives. - Added tests for whitboard pattern switched to failsafe plugin

--- itest/itest-whiteboard/src/test/java/org/ops4j/pax/web/itest/whiteboard/WhiteboardIntegrationTest.java
+++ itest/itest-whiteboard/src/test/java/org/ops4j/pax/web/itest/whiteboard/WhiteboardIntegrationTest.java
@@ -1,51 +1,40 @@
-package org.ops4j.pax.web.itest.war;
+package org.ops4j.pax.web.itest.whiteboard;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
 import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 import static org.ops4j.pax.exam.CoreOptions.wrappedBundle;
+import static org.ops4j.pax.exam.CoreOptions.waitForFrameworkStartup;
 import static org.ops4j.pax.exam.MavenUtils.asInProject;
 import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.compendiumProfile;
 import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.configProfile;
 import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.logProfile;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
 
 import java.io.IOException;
-import java.util.Dictionary;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Inject;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
-import org.ops4j.pax.web.service.spi.WebEvent;
-import org.ops4j.pax.web.service.spi.WebListener;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 
-
 /**
- * @author Achim Nierbeck
+ * @author Toni Menzel (tonit)
+ * @since Mar 3, 2009
  */
 @RunWith(JUnit4TestRunner.class)
-public class WarIntegrationTest {
-
-	Log LOG = LogFactory.getLog(WarIntegrationTest.class);
-
-	private Bundle installWarBundle;
-
-	private WebListener webListener;
+public class WhiteboardIntegrationTest {
 	
-
 	@Inject
 	private BundleContext bundleContext = null;
 	
@@ -113,42 +102,13 @@ public class WarIntegrationTest {
 					wrappedBundle(mavenBundle("commons-httpclient",
 							"commons-httpclient", "3.1"))
 	// enable for debugging
-	//				,
-	//				vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"),
-	//				waitForFrameworkStartup()
+//					,
+//					vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"),
+//					waitForFrameworkStartup()
 	
 			);
 		}
-
-	@Before
-	public void setUp() throws BundleException, InterruptedException {
-		LOG.info("Setting up test");
-		webListener = new WebListenerImpl();
-		bundleContext.registerService(WebListener.class.getName(), webListener,
-				null);
-		String bundlePath = WEB_BUNDLE
-				+ "mvn:org.ops4j.pax.web.samples/war/1.1.0-SNAPSHOT/war?"
-				+ WEB_CONTEXT_PATH + "=/war";
-		installWarBundle = bundleContext.installBundle(bundlePath);
-		installWarBundle.start();
-
-		int count = 0;
-		while (!((WebListenerImpl) webListener).gotEvent() && count < 50) {
-			synchronized (this) {
-				this.wait(100);
-				count++;
-			}
-		}
-	}
-
-	@After
-	public void tearDown() throws BundleException {
-		if (installWarBundle != null) {
-			installWarBundle.stop();
-			installWarBundle.uninstall();
-		}
-	}
-
+	
 	/**
 	 * You will get a list of bundles installed by default plus your testcase,
 	 * wrapped into a bundle called pax-exam-probe
@@ -156,32 +116,25 @@ public class WarIntegrationTest {
 	@Test
 	public void listBundles() {
 		for (Bundle b : bundleContext.getBundles()) {
-			if (b.getState() != Bundle.ACTIVE)
-				fail("Bundle should be active: " + b);
-
-			Dictionary headers = b.getHeaders();
-			String ctxtPath = (String) headers.get(WEB_CONTEXT_PATH);
-			if (ctxtPath != null)
-				System.out.println("Bundle " + b.getBundleId() + " : "
-						+ b.getSymbolicName() + " : " + ctxtPath);
-			else
-				System.out.println("Bundle " + b.getBundleId() + " : "
-						+ b.getSymbolicName());
+			System.out.println("Bundle " + b.getBundleId() + " : "
+					+ b.getSymbolicName());
 		}
 
 	}
 
 	@Test
-	public void testWebContextPath() throws Exception {
-
-		testWebPath("http://127.0.0.1:8080/war/wc", "<h1>Hello World</h1>");
-			
-		testWebPath("http://127.0.0.1:8080/war/wc/example", "<h1>Hello World</h1>");
-			
-		testWebPath("http://127.0.0.1:8080/war/wc/sn", "<h1>Hello World</h1>");
-
+	public void testWhiteBoardJar() throws BundleException, InterruptedException, HttpException, IOException {
+		String bundlePath = "mvn:org.ops4j.pax.web.samples/whiteboard/1.1.0-SNAPSHOT";
+		Bundle installWarBundle = bundleContext.installBundle(bundlePath);
+		installWarBundle.start();
+		
+		while (installWarBundle.getState() != Bundle.ACTIVE) {
+			this.wait(100);
+		}
+		
+		testWebPath("http://127.0.0.1:8080/whiteboard", "");
 	}
-
+	
 	/**
 	 * @return
 	 * @throws IOException
@@ -201,20 +154,4 @@ public class WarIntegrationTest {
 				get.releaseConnection();
 		}
 	}
-
-	private class WebListenerImpl implements WebListener {
-
-		private boolean event = false;
-
-		public void webEvent(WebEvent event) {
-			LOG.info("Got event: " + event);
-			this.event = true;
-		}
-
-		public boolean gotEvent() {
-			return event;
-		}
-
-	}
-
 }
