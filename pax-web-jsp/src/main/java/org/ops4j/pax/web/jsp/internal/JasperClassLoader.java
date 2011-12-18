902d1c19e3fac62807de8086a3e69bac3fd07ee4
Marc Klinger <mklinger@nightlabs.de>
2011-12-18 16:45:32 +0100

    [PAXWEB-289] - Fixed regression by falling back to importedBundle.getEntry("/") on MalformedURLException

--- pax-web-jsp/src/main/java/org/ops4j/pax/web/jsp/internal/JasperClassLoader.java
+++ pax-web-jsp/src/main/java/org/ops4j/pax/web/jsp/internal/JasperClassLoader.java
@@ -16,7 +16,6 @@
  */
 package org.ops4j.pax.web.jsp.internal;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -43,6 +42,7 @@ import org.slf4j.LoggerFactory;
  *
  * @author Alin Dreghiciu
  * @author Raul Kripalani
+ * @author Marc Klinger - mklinger[at]nightlabs[dot]de
  * @since 0.3.0 January 08, 2008
  */
 public final class JasperClassLoader
@@ -172,16 +172,27 @@ public final class JasperClassLoader
     private static List<URL> getLocationsOfBundlesInClassSpace(Bundle bundle) {
     	List<URL> urls = new ArrayList<URL>();
     	List<Bundle> importedBundles = getBundlesInClassSpace(bundle);
-    	
-    	try {
-	    	for (Bundle importedBundle : importedBundles) {
-	    		URL url = new URL(importedBundle.getLocation()); 
-				urls.add(url);
-	    	}
-        } catch (MalformedURLException e) {
-			LOG.warn("Exception while calculating location of imported bundles", e);
-		}
+    	for (Bundle importedBundle : importedBundles) {
+    		URL url = getLocationOfBundle(importedBundle);
+            if (url != null) {
+            	urls.add(url);
+            }
+    	}
         return urls;
+	}
+
+	private static URL getLocationOfBundle(Bundle importedBundle) {
+		URL url = null;
+		try {
+			url = new URL(importedBundle.getLocation()); 
+		} catch (MalformedURLException e) {
+			try {
+			url = importedBundle.getEntry("/");
+			} catch (Exception e2) {
+				LOG.warn("Exception while calculating location of bundle", e);
+			}
+		}
+		return url;
 	}
     
     /**
