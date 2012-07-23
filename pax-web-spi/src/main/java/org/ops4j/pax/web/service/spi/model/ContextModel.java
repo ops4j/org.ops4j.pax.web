26cfd7fad9a3c0d927939129c4cc98f14441e912
Tuomas Kiviaho <tuomas.kiviaho@iki.fi>
2012-07-23 15:14:20 +0300

    - added new it covering multiple http context registrations per bundle - Postponed creation of default http context mapping to avoid unnecessary registration/unregistration cycles when defined mapping would override the default one right away. - Avoid 'http context already in use' when configuration change would not have an impact on registered servlets/etc. - ContextModel did not properly clean context parameters if new set of paramaters was empty parameters were no incoming parameters parameters were

--- pax-web-spi/src/main/java/org/ops4j/pax/web/service/spi/model/ContextModel.java
+++ pax-web-spi/src/main/java/org/ops4j/pax/web/service/spi/model/ContextModel.java
@@ -156,9 +156,9 @@ public class ContextModel extends Identity
     @SuppressWarnings("rawtypes")
 	public void setContextParams( final Dictionary contextParams )
     {
+        m_contextParams.clear();
         if( contextParams != null && !contextParams.isEmpty() )
         {
-            m_contextParams.clear();
             final Enumeration keys = contextParams.keys();
             while( keys.hasMoreElements() )
             {
@@ -170,8 +170,8 @@ public class ContextModel extends Identity
                 }
                 m_contextParams.put( (String) key, (String) value );
             }
+            m_contextName = m_contextParams.get( WebContainerConstants.CONTEXT_NAME );
         }
-        m_contextName = m_contextParams.get( WebContainerConstants.CONTEXT_NAME );
         if( m_contextName != null )
         {
             m_contextName = m_contextName.trim();
