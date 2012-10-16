fc600218ef9f617344867a2c88a2a72b71bf7430
Achim Nierbeck <bcanhome@googlemail.com>
2012-10-17 00:15:44 +0200

    Added test with std. http-service
    Conflicts:
    	itest/src/test/java/org/ops4j/pax/web/itest/RootAliasIntegrationTest.java

---
+++ itest/src/test/java/org/ops4j/pax/web/itest/RootAliasIntegrationTest.java
@@ -0,0 +1,129 @@
+package org.ops4j.pax.web.itest;
+
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.web.extender.samples.whiteboard.internal.WhiteboardFilter;
+import org.ops4j.pax.web.extender.samples.whiteboard.internal.WhiteboardServlet;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+
+/**
+ * @author Toni Menzel (tonit)
+ * @since Mar 3, 2009
+ */
+@RunWith(JUnit4TestRunner.class)
+public class RootAliasIntegrationTest extends ITestBase {
+
+	private ServiceRegistration servletRoot;
+	private ServiceRegistration servletSecond;
+	private ServiceReference serviceReference;
+	private HttpService httpService;
+
+	@Configuration
+	public static Option[] configure() {
+		return baseConfigure();
+	}
+
+	@Before
+	public void setUp() throws BundleException, InterruptedException, ServletException, NamespaceException {
+		
+		
+		servletRoot = registerServletWhiteBoard("/myRoot");
+		servletSecond = registerServletWhiteBoard("/myRoot/second");
+		
+		serviceReference = bundleContext.getServiceReference("org.osgi.service.http.HttpService");
+		
+		httpService = (HttpService) bundleContext.getService(serviceReference);
+		
+		registerServlet("/secondRoot");
+		registerServlet("/secondRoot/third");
+	}
+	
+	private ServiceRegistration registerServletWhiteBoard(final String path) throws ServletException {
+		
+		Dictionary<String, String> initParams = new Hashtable<String, String>();
+		initParams.put("alias", path);
+
+        return bundleContext.registerService(Servlet.class.getName(),
+        		new HttpServlet() {
+
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                resp.getOutputStream().write(path.getBytes());
+            }
+        }, initParams);
+        
+    }
+	
+	private void registerServlet(final String path) throws ServletException, NamespaceException {
+        httpService.registerServlet(path, new HttpServlet() {
+
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                resp.getOutputStream().write(path.getBytes());
+            }
+        }, null, null);
+        System.out.println("registered: " + path);
+    }
+
+    private void unregisterServlet(String path) {
+        httpService.unregister(path);
+        System.out.println("unregistered: " + path);
+    }
+
+	@After
+	public void tearDown() throws BundleException {
+		servletRoot.unregister();
+		servletSecond.unregister();
+		
+
+		unregisterServlet("/secondRoot");
+		unregisterServlet("/secondRoot/third");
+	}
+
+	@Test
+	public void testWhiteBoardSlash() throws BundleException,
+			InterruptedException, IOException {
+		testWebPath("http://127.0.0.1:8181/myRoot", "myRoot");
+
+		testWebPath("http://127.0.0.1:8181/myRoot/second", "myRoot/second");
+		
+		testWebPath("http://127.0.0.1:8181/myRoot/wrong", "myRoot");
+		
+		testWebPath("http://127.0.0.1:8181/secondRoot", "secondRoot");
+
+		testWebPath("http://127.0.0.1:8181/secondRoot/third", "secondRoot/third");
+		
+		testWebPath("http://127.0.0.1:8181/secondRoot/wrong", "secondRoot");
+
+	}
+	
+	
+
+}
