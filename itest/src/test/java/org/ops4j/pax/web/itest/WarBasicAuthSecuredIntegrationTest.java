5859c4b5e2fccdc6d9b8f03b95c13a9743bf055b
Achim Nierbeck <bcanhome@googlemail.com>
2012-10-17 23:40:24 +0200

    [PAXWEB-432] - Non trusted client call raises ClassNotFoundException

--- itest/src/test/java/org/ops4j/pax/web/itest/WarBasicAuthSecuredIntegrationTest.java
+++ itest/src/test/java/org/ops4j/pax/web/itest/WarBasicAuthSecuredIntegrationTest.java
@@ -2,9 +2,13 @@ package org.ops4j.pax.web.itest;
 
 import static org.junit.Assert.fail;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
 import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Dictionary;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -13,7 +17,9 @@ import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.OptionUtils;
 import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
 import org.ops4j.pax.web.service.spi.WebEvent;
 import org.ops4j.pax.web.service.spi.WebListener;
 import org.osgi.framework.Bundle;
@@ -21,38 +27,54 @@ import org.osgi.framework.BundleException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * @author Achim Nierbeck
  */
 @RunWith(JUnit4TestRunner.class)
-public class WebContainerSecuredIntegrationTest extends ITestBase {
+public class WarBasicAuthSecuredIntegrationTest extends ITestBase {
 
- Logger LOG = LoggerFactory.getLogger(WebContainerSecuredIntegrationTest.class);
+	Logger LOG = LoggerFactory.getLogger(WarBasicAuthSecuredIntegrationTest.class);
 
 	private Bundle installWarBundle;
 
 	private WebListener webListener;
-	
+
 	@Configuration
-	public static Option[] configure() {
-		return OptionUtils.combine(baseConfigure(),
+	public static Option[] configuration() {
+		return OptionUtils.combine(
+				baseConfigure(),
 				systemProperty("org.osgi.service.http.secure.enabled").value("true"),
 				systemProperty("org.ops4j.pax.web.ssl.keystore").value("src/test/resources/keystore"),
 				systemProperty("org.ops4j.pax.web.ssl.password").value("password"),
 				systemProperty("org.ops4j.pax.web.ssl.keypassword").value("password"),
-				systemProperty("org.ops4j.pax.web.ssl.clientauthneeded").value("required"));
+				systemProperty("org.ops4j.pax.web.ssl.clientauthneeded").value("required"),
+				mavenBundle()
+					.groupId("org.ops4j.pax.web.samples")
+					.artifactId("jetty-auth-config-fragment")
+					.version(getProjectVersion()).noStart()
+				);
 	}
-
 
 	@Before
 	public void setUp() throws BundleException, InterruptedException {
-		String bundlePath = "mvn:org.ops4j.pax.web.samples/helloworld-wc/" + getProjectVersion();
+		LOG.info("Setting up test");
+
+		webListener = new WebListenerImpl();
+		bundleContext.registerService(WebListener.class.getName(), webListener,
+				null);
+		String bundlePath = WEB_BUNDLE
+				+ "mvn:org.ops4j.pax.web.samples/war-authentication/"
+				+ getProjectVersion() + "/war?"
+				+ WEB_CONTEXT_PATH + "=/war-authentication";
 		installWarBundle = bundleContext.installBundle(bundlePath);
 		installWarBundle.start();
 
-		while (installWarBundle.getState() != Bundle.ACTIVE) {
-			this.wait(100);
+		int count = 0;
+		while (!((WebListenerImpl) webListener).gotEvent() && count < 50) {
+			synchronized (this) {
+				this.wait(100);
+				count++;
+			}
 		}
 	}
 
@@ -88,10 +110,39 @@ public class WebContainerSecuredIntegrationTest extends ITestBase {
 	}
 
 	@Test
-	public void testWebContextPath() throws Exception {
+	public void testWC() throws Exception {
 
-		testSecureWebPath("https://127.0.0.1:8443/helloworld/wc", "<h1>Hello World</h1>");
-			
+		testSecureWebPath("https://127.0.0.1:8443/war-authentication/wc",
+				"<h1>Hello World</h1>");
+
+	}
+
+	@Test
+	public void testWC_example() throws Exception {
+
+		testSecureWebPath("https://127.0.0.1:8443/war-authentication/wc/example",
+				"Unauthorized", 401, false);
+
+		testSecureWebPath("https://127.0.0.1:8443/war-authentication/wc/example",
+				"<h1>Hello World</h1>", 200, true);
+
+	}
+
+	@Test
+	public void testWC_SN() throws Exception {
+
+		testSecureWebPath("https://127.0.0.1:8443/war-authentication/wc/sn",
+				"<h1>Hello World</h1>");
+
+	}
+
+	@Test
+	public void testSlash() throws Exception {
+
+		LOG.info("Starting test ...");
+		testSecureWebPath("https://127.0.0.1:8443/war-authentication/",
+				"<h1>Hello World</h1>");
+		LOG.info("...Done");
 	}
 
 	private class WebListenerImpl implements WebListener {
