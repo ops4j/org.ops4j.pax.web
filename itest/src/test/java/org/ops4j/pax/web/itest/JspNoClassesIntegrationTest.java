f71c3ff174e78a61076f1bcce5940269ec5ccaa2
Achim Nierbeck <bcanhome@googlemail.com>
2012-11-11 22:15:06 +0100

    Some more e improvements for testing
    Signed-off-by: Achim Nierbeck <bcanhome@googlemail.com>

--- itest/src/test/java/org/ops4j/pax/web/itest/JspNoClassesIntegrationTest.java
+++ itest/src/test/java/org/ops4j/pax/web/itest/JspNoClassesIntegrationTest.java
@@ -7,6 +7,8 @@ import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.web.service.spi.WebEvent;
+import org.ops4j.pax.web.service.spi.WebListener;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleException;
 import org.slf4j.Logger;
@@ -24,6 +26,8 @@ public class JspNoClassesIntegrationTest extends ITestBase {
 	Logger LOG = LoggerFactory.getLogger(JspNoClassesIntegrationTest.class);
 
 	private Bundle installWarBundle;
+	
+	private WebListener webListener;
 
 	@Configuration
 	public static Option[] configure() {
@@ -33,12 +37,25 @@ public class JspNoClassesIntegrationTest extends ITestBase {
 
 	@Before
 	public void setUp() throws BundleException, InterruptedException {
+		
+		webListener = new WebListenerImpl();
+		bundleContext.registerService(WebListener.class.getName(), webListener,
+				null);
+
 		String bundlePath = "mvn:org.ops4j.pax.web.samples/helloworld-jsp-noclasses/" + getProjectVersion();
 		installWarBundle = bundleContext.installBundle(bundlePath);
 		installWarBundle.start();
 
 		while (installWarBundle.getState() != Bundle.ACTIVE) {
 			this.wait(100);
+		}
+		
+		int count = 0;
+		while (!((WebListenerImpl) webListener).gotEvent() && count < 100) {
+			synchronized (this) {
+				this.wait(100);
+				count++;
+			}
 		}
 	}
 
@@ -56,4 +73,21 @@ public class JspNoClassesIntegrationTest extends ITestBase {
 		testWebPath("http://localhost:8181/jspnc/welcome.jsp", "Welcome");
 			
 	}
+	
+	private class WebListenerImpl implements WebListener {
+
+		private boolean event = false;
+
+		public void webEvent(WebEvent event) {
+			LOG.info("Got event: " + event);
+			if (event.getType() == 2)
+				this.event = true;
+		}
+
+		public boolean gotEvent() {
+			return event;
+		}
+
+	}
+	
 }
