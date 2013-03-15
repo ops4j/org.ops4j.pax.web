d08a597eeb16b3e1f8bb36686ee109f83e81522e
Harald Wellmann <harald.wellmann@gmx.de>
2013-03-15 23:01:57 +0100

    [PAXWEB-546] Timing issue in HttpServiceWithConfigAdminIntegrationTest
    helloworld-hs Activator did not wait for HttpService

--- samples/helloworld-hs/src/main/java/org/ops4j/pax/web/samples/helloworld/hs/internal/Activator.java
+++ samples/helloworld-hs/src/main/java/org/ops4j/pax/web/samples/helloworld/hs/internal/Activator.java
@@ -19,11 +19,16 @@ package org.ops4j.pax.web.samples.helloworld.hs.internal;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
+import javax.servlet.ServletException;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.http.HttpContext;
 import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * Hello World Activator.
@@ -31,65 +36,77 @@ import org.osgi.service.http.HttpService;
  * @author Alin Dreghiciu
  * @since 0.3.0, January 02, 2007
  */
-public final class Activator implements BundleActivator {
+public final class Activator implements BundleActivator,
+	ServiceTrackerCustomizer<HttpService, HttpService> {
 
-	/**
-	 * HttpService reference.
-	 */
-	private ServiceReference<HttpService> httpServiceRef;
+	private BundleContext bc;
+
+	private ServiceTracker<HttpService, HttpService> tracker;
 
 	/**
 	 * Called when the OSGi framework starts our bundle
 	 */
 	public void start(BundleContext bc) throws Exception {
-		httpServiceRef = bc.getServiceReference(HttpService.class);
-		if (httpServiceRef != null) {
-			final HttpService httpService = (HttpService) bc
-					.getService(httpServiceRef);
-			if (httpService != null) {
-				// create a default context to share between registrations
-				final HttpContext httpContext = httpService
-						.createDefaultHttpContext();
-				// register the hello world servlet
-				final Dictionary<String, Object> initParams = new Hashtable<String, Object>();
-				initParams.put("from", "HttpService");
-				httpService.registerServlet("/helloworld/hs", // alias
-						new HelloWorldServlet("/helloworld/hs"), // registered
-																	// servlet
-						initParams, // init params
-						httpContext // http context
-						);
-				httpService.registerServlet("/*", // alias - using /* as alias
-													// does work around Issue
-													// PAXWEB-108
-						new HelloWorldServlet("/"), // registered servlet
-						initParams, // init params
-						httpContext // http context
-						);
-				// register images as resources
-				httpService
-						.registerResources("/images", "/images", httpContext);
-			}
-		}
+		this.bc = bc;
+		tracker = new ServiceTracker<HttpService, HttpService>(bc, HttpService.class, this);
+		tracker.open();
 	}
 
 	/**
 	 * Called when the OSGi framework stops our bundle
 	 */
 	public void stop(BundleContext bc) throws Exception {
-		if (httpServiceRef != null) {
-			try {
-				HttpService service = (HttpService) bc
-						.getService(httpServiceRef);
-				service.unregister("/helloworld/hs");
-				service.unregister("/*");
-				service.unregister("/images");
-			} catch (Exception e) { //CHECKSTYLE:SKIP
+		tracker.close();
+	}
 
-			} finally {
-				bc.ungetService(httpServiceRef);
-				httpServiceRef = null;
+	@Override
+	public HttpService addingService(ServiceReference<HttpService> reference) {
+		final HttpService httpService = (HttpService) bc.getService(reference);
+		if (httpService != null) {
+			// create a default context to share between registrations
+			final HttpContext httpContext = httpService.createDefaultHttpContext();
+			// register the hello world servlet
+			final Dictionary<String, Object> initParams = new Hashtable<String, Object>();
+			initParams.put("from", "HttpService");
+			try {
+				httpService.registerServlet("/helloworld/hs", // alias
+					new HelloWorldServlet("/helloworld/hs"), // registered servlet
+					initParams,
+					httpContext
+					);
+				httpService.registerServlet("/*", // alias - using /* as alias
+					// does work around issue PAXWEB-108
+					new HelloWorldServlet("/"), // registered servlet
+					initParams,
+					httpContext
+					);
+				// register images as resources
+				httpService.registerResources("/images", "/images", httpContext);
 			}
+			catch (ServletException e) {
+				e.printStackTrace();
+			}
+			catch (NamespaceException e) {
+				e.printStackTrace();
+			}
+		}
+		return httpService;
+	}
+
+	@Override
+	public void modifiedService(ServiceReference<HttpService> reference, HttpService service) {
+		// ignore
+	}
+
+	@Override
+	public void removedService(ServiceReference<HttpService> reference, HttpService service) {
+		try {
+			service.unregister("/helloworld/hs");
+			service.unregister("/*");
+			service.unregister("/images");
+		}
+		catch (Exception e) { // CHECKSTYLE:SKIP
+
 		}
 	}
 }
