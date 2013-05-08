9f414016ef72b732777c58e9f35a1c03dab899cb
anierbeck <bcanhome@googlemail.com>
2013-05-08 15:59:40 +0200

    [PAXWEB-566] - Add a Blueprint-Whiteboard Sample for usage in Karaf

---
+++ pax-web-itest-karaf/src/test/java/org/ops4j/pax/web/itest/karaf/WhiteboardIntegrationTest.java
@@ -0,0 +1,209 @@
+package org.ops4j.pax.web.itest.karaf;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+
+import org.apache.http.Header;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.web.extender.samples.whiteboard.internal.WhiteboardServlet;
+import org.ops4j.pax.web.extender.whiteboard.HttpContextMapping;
+import org.ops4j.pax.web.extender.whiteboard.ServletMapping;
+import org.ops4j.pax.web.extender.whiteboard.runtime.DefaultHttpContextMapping;
+import org.ops4j.pax.web.extender.whiteboard.runtime.DefaultServletMapping;
+import org.ops4j.pax.web.service.spi.WebEvent;
+import org.ops4j.pax.web.service.spi.WebListener;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.ServiceRegistration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Toni Menzel (tonit)
+ * @since Mar 3, 2009
+ */
+@RunWith(JUnit4TestRunner.class)
+public class WhiteboardIntegrationTest extends KarafBaseTest {
+
+	private static Logger LOG = LoggerFactory.getLogger(WhiteboardIntegrationTest.class);
+
+	private WebListenerImpl webListener;
+	
+	private Bundle installWarBundle;
+
+	@Configuration
+	public Option[] config() {
+		return baseConfig();
+	}
+	
+	@Before
+	public void setUp() throws BundleException, InterruptedException, ClientProtocolException, IOException {
+		int count = 0;
+		while (!checkServer() && count < 200) {
+			synchronized (this) {
+				this.wait(100);
+				count++;
+			}
+		}
+		LOG.info("waiting for Server took {} ms", (count * 1000));
+
+		String bundlePath = "mvn:org.ops4j.pax.web.samples/whiteboard-blueprint/"
+				+ getProjectVersion();
+		installWarBundle = bundleContext.installBundle(bundlePath);
+		installWarBundle.start();
+		
+		webListener = new WebListenerImpl();
+
+		int failCount = 0;
+		while (installWarBundle.getState() != Bundle.ACTIVE) {
+			Thread.sleep(500);
+			if (failCount > 500)
+				throw new RuntimeException(
+						"Required whiteboard-blueprint bundle is never active");
+			failCount++;
+		}
+
+		count = 0;
+		while (!((WebListenerImpl) webListener).gotEvent() && count < 100) {
+			synchronized (this) {
+				this.wait(100);
+				count++;
+			}
+		}
+		LOG.info("waiting for Server took {} ms", (count * 1000));
+		
+	}
+
+	@After
+	public void tearDown() throws BundleException {
+		if (installWarBundle != null) {
+			installWarBundle.stop();
+			installWarBundle.uninstall();
+		}
+	}
+
+	/**
+	 * You will get a list of bundles installed by default plus your testcase,
+	 * wrapped into a bundle called pax-exam-probe
+	 */
+	@Test
+	public void listBundles() {
+		for (Bundle b : bundleContext.getBundles()) {
+			System.out.println("Bundle " + b.getBundleId() + " : "
+					+ b.getSymbolicName());
+		}
+
+	}
+
+	@Test
+	public void testWhiteBoardRoot() throws Exception {
+		testWebPath("http://127.0.0.1:8181/root", "Hello Whiteboard Extender");
+	}
+
+	@Test
+	public void testWhiteBoardSlash() throws Exception {
+		testWebPath("http://127.0.0.1:8181/", "Welcome to the Welcome page");
+	}
+
+	@Test
+	public void testWhiteBoardForbidden() throws Exception {
+		testWebPath("http://127.0.0.1:8181/forbidden", "", 401, false);
+	}
+
+	@Test
+	public void testWhiteBoardFiltered() throws Exception {
+		testWebPath("http://127.0.0.1:8181/filtered", "Filter was there before");
+	}
+
+	@Test
+	public void testImage() throws Exception {
+		HttpResponse httpResponse = getHttpResponse(
+				"http://127.0.0.1:8181/images/ops4j.png", false, null);
+		Header header = httpResponse.getFirstHeader(HttpHeaders.CONTENT_TYPE);
+		assertEquals("image/png", header.getValue());
+	}
+
+	@Test
+	public void test404() throws Exception {
+		testWebPath("http://127.0.0.1:8181/doesNotExist",
+				"<title>Default 404 page</title>", 404, false);
+	}
+	
+	@Test
+	public void testResourceMapping() throws Exception {
+		HttpResponse httpResponse = getHttpResponse(
+				"http://127.0.0.1:8181/whiteboardresources/ops4j.png", false, null);
+		Header header = httpResponse.getFirstHeader(HttpHeaders.CONTENT_TYPE);
+		assertEquals("image/png", header.getValue());
+	}
+	
+	@Test
+	public void testJspMapping() throws Exception {
+		testWebPath("http://127.0.0.1:8181/jsp/simple.jsp", "<h1>Hello World</h1>");
+	}
+	
+	@Test
+	public void testTldJsp() throws Exception {
+		testWebPath("http://127.0.0.1:8181/jsp/using-tld.jsp", "Hello World");
+	}
+
+	@Test
+	public void testMultipleContextMappings() throws Exception {
+		BundleContext bundleContext = installWarBundle.getBundleContext();
+		DefaultHttpContextMapping httpContextMapping = new DefaultHttpContextMapping();
+		httpContextMapping.setHttpContextId("alternative");
+		httpContextMapping.setPath("alternative");
+		ServiceRegistration<HttpContextMapping> httpContextMappingRegistration = bundleContext
+				.registerService(HttpContextMapping.class,
+						httpContextMapping, null);
+		try {
+			Servlet servlet = new WhiteboardServlet("/alias");
+			DefaultServletMapping servletMapping = new DefaultServletMapping();
+			servletMapping.setServlet(servlet);
+			servletMapping.setAlias("/alias");
+			String httpContextId = httpContextMapping.getHttpContextId();
+			servletMapping.setHttpContextId(httpContextId);
+			ServiceRegistration<ServletMapping> servletRegistration = bundleContext
+					.registerService(ServletMapping.class,
+							servletMapping, null);
+			try {
+				testWebPath("http://127.0.0.1:8181/alternative/alias",
+						"Hello Whiteboard Extender");
+			} finally {
+				servletRegistration.unregister();
+			}
+		} finally {
+			httpContextMappingRegistration.unregister();
+		}
+	}
+	
+	
+	private class WebListenerImpl implements WebListener {
+
+		private boolean event = false;
+
+		public void webEvent(WebEvent event) {
+			LOG.info("Got event: " + event);
+			if (event.getType() == 2)
+				this.event = true;
+		}
+
+		public boolean gotEvent() {
+			return event;
+		}
+	}
+
+}
