8b83425eb2f03a3ac9b670dde277f483377bc583
anierbeck <bcanhome@googlemail.com>
2013-05-22 11:35:21 +0200

    [PAXWEB-578] - Upgrade to Pax-Exam 3.1.0-SNAPSHOT

--- itest/src/test/java/org/ops4j/pax/web/itest/ITestBase.java
+++ itest/src/test/java/org/ops4j/pax/web/itest/ITestBase.java
@@ -96,7 +96,7 @@ public class ITestBase {
 						"false"),
 				// frameworkProperty("felix.log.level").value("4"),
 				systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level")
-						.value("WARN"),
+						.value("INFO"),
 				systemProperty("org.osgi.service.http.hostname").value(
 						"127.0.0.1"),
 				systemProperty("org.osgi.service.http.port").value("8181"),
