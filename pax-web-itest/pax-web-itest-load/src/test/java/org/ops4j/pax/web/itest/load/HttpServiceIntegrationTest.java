b27739ed38c342422c3b5543a97eb7c34194872b
anierbeck <bcanhome@googlemail.com>
2013-08-01 23:21:25 +0200

    [PAXWEB-611] - Seperate Tomcat from Jetty Itests

--- pax-web-itest/pax-web-itest-load/src/test/java/org/ops4j/pax/web/itest/load/HttpServiceIntegrationTest.java
+++ pax-web-itest/pax-web-itest-load/src/test/java/org/ops4j/pax/web/itest/load/HttpServiceIntegrationTest.java
@@ -9,6 +9,7 @@ import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.EagerSingleStagedReactorFactory;
+import org.ops4j.pax.web.itest.base.VersionUtil;
 
 /**
  * @author Toni Menzel (tonit)
@@ -24,14 +25,15 @@ public class HttpServiceIntegrationTest /*extends ITestBase*/ {
 		return options(systemProperty("org.osgi.service.http.port").value("8181"),
 	            frameworkProperty("osgi.console").value("6666"),
 
-	            mavenBundle("org.ops4j.pax.web", "pax-web-spi").version("2.0.2"),
-	            mavenBundle("org.ops4j.pax.web", "pax-web-api").version("2.0.2"),
-	            mavenBundle("org.ops4j.pax.web", "pax-web-extender-war").version("2.0.2"),
-	            mavenBundle("org.ops4j.pax.web", "pax-web-extender-whiteboard").version("2.0.2"),
-	            mavenBundle("org.ops4j.pax.web", "pax-web-jetty").version("2.0.2"),
-	            mavenBundle("org.ops4j.pax.web", "pax-web-runtime").version("2.0.2"),
-	            mavenBundle("org.ops4j.pax.web", "pax-web-jsp").version("2.0.2"),
-	            mavenBundle("org.eclipse.jdt.core.compiler", "ecj").version("3.5.1"),
+//	            mavenBundle("org.ops4j.pax.web.itest", "pax-web-itest-base").versionAsInProject(),
+	            mavenBundle("org.ops4j.pax.web", "pax-web-spi").versionAsInProject(),
+	            mavenBundle("org.ops4j.pax.web", "pax-web-api").versionAsInProject(),
+	            mavenBundle("org.ops4j.pax.web", "pax-web-extender-war").versionAsInProject(),
+	            mavenBundle("org.ops4j.pax.web", "pax-web-extender-whiteboard").versionAsInProject(),
+	            mavenBundle("org.ops4j.pax.web", "pax-web-jetty").versionAsInProject(),
+	            mavenBundle("org.ops4j.pax.web", "pax-web-runtime").versionAsInProject(),
+	            mavenBundle("org.ops4j.pax.web", "pax-web-jsp").versionAsInProject(),
+	            mavenBundle("org.eclipse.jdt.core.compiler", "ecj").versionAsInProject(),
 	            mavenBundle("org.eclipse.jetty", "jetty-util").version("8.1.4.v20120524"),
 	            mavenBundle("org.eclipse.jetty", "jetty-io").version("8.1.4.v20120524"),
 	            mavenBundle("org.eclipse.jetty", "jetty-http").version("8.1.4.v20120524"),
@@ -46,7 +48,7 @@ public class HttpServiceIntegrationTest /*extends ITestBase*/ {
 	            mavenBundle("org.slf4j", "slf4j-api", "1.6.4"),
 	            mavenBundle("org.slf4j", "slf4j-simple", "1.6.4").noStart(),
 
-	            mavenBundle().groupId("org.ops4j.pax.web.samples").artifactId("helloworld-hs").version(getProjectVersion())
+	            mavenBundle().groupId("org.ops4j.pax.web.samples").artifactId("helloworld-hs").version(VersionUtil.getProjectVersion())
 
 	        );
 	}
