28c99f7e3efc6c8415baa7c6246750953e6b25d5
Guillaume Yziquel <guillaume.yziquel@crossing-tech.com>
2013-09-27 17:06:49 +0200

    [PAXWEB-623] Removing unused imports.

--- samples/helloworld-wc/src/main/java/org/ops4j/pax/web/samples/helloworld/wc/internal/HelloWorldStartupTalkativeServlet.java
+++ samples/helloworld-wc/src/main/java/org/ops4j/pax/web/samples/helloworld/wc/internal/HelloWorldStartupTalkativeServlet.java
@@ -18,13 +18,11 @@ package org.ops4j.pax.web.samples.helloworld.wc.internal;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Hello World Servlet.
