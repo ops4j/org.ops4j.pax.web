3e09395957ee74824d395a273fe5e31a3800f28b
anierbeck <bcanhome@googlemail.com>
2013-10-07 21:23:33 +0200

    [PAXWEB-154] - Allow specification of Filter behavior during RequestDispatcher processing (i.e. web.xml filter-mapping/dispatcher) ---- minor enhancement

--- samples/jsp-filter/src/main/java/org/ops4j/pax/web/jsp/filter/test/TestFilter.java
+++ samples/jsp-filter/src/main/java/org/ops4j/pax/web/jsp/filter/test/TestFilter.java
@@ -37,8 +37,7 @@ public class TestFilter implements Filter {
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-
+		LOG.debug("TestFilter - init: {}", arg0);
 	}
 
 }
