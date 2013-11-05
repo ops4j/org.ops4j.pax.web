495b1d0829f7af40712cd51c21ad9873cc6c5558
anierbeck <bcanhome@googlemail.com>
2013-11-05 21:01:48 +0100

    [PAXWEB-637] - ConcurrentModificationException when unregistering JSPs

--- pax-web-runtime/src/main/java/org/ops4j/pax/web/service/internal/HttpServiceStarted.java
+++ pax-web-runtime/src/main/java/org/ops4j/pax/web/service/internal/HttpServiceStarted.java
@@ -670,6 +670,7 @@ class HttpServiceStarted implements StoppableHttpService {
 					"Jsp support is not enabled for http context ["
 							+ httpContext + "]");
 		}
+		List<Servlet> unregisteredJSPServlets = new ArrayList<Servlet>();
 		for (Iterator<Map.Entry<Servlet, String[]>> jspServlets = contextModel
 				.getJspServlets().entrySet().iterator(); jspServlets.hasNext();) {
 			Map.Entry<Servlet, String[]> entry = jspServlets.next();
@@ -679,9 +680,12 @@ class HttpServiceStarted implements StoppableHttpService {
 				try {
 					unregisterServlet(jspServlet);
 				} finally {
-					jspServlets.remove();
+					unregisteredJSPServlets.add(jspServlet);
 				}
 			}
+		}
+		for (Servlet unregisteredJSPServlet : unregisteredJSPServlets) {
+			contextModel.getJspServlets().remove(unregisteredJSPServlet);
 		}
 	}
 
