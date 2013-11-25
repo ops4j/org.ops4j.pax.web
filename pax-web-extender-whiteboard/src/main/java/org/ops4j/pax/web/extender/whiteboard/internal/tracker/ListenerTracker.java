cd5a5b1bd1d8338005cc01eb475d92b27a055649
Guillaume Nodet <gnodet@gmail.com>
2013-11-25 15:24:32 +0100

    [PAXWEB-644] Fix bad commit

--- pax-web-extender-whiteboard/src/main/java/org/ops4j/pax/web/extender/whiteboard/internal/tracker/ListenerTracker.java
+++ pax-web-extender-whiteboard/src/main/java/org/ops4j/pax/web/extender/whiteboard/internal/tracker/ListenerTracker.java
@@ -66,7 +66,7 @@ public class ListenerTracker extends
 			final ExtenderContext extenderContext,
 			final BundleContext bundleContext) {
 		return new ListenerTracker(extenderContext, bundleContext)
-				.create(ServletContextListener.class);
+				.create(EventListener.class);
 	}
 
 	/**
