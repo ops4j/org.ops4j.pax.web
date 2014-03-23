6e49127a30f7a6dd000218734d39c4c17eb86716
anierbeck <bcanhome@googlemail.com>
2014-03-23 23:13:22 +0100

    [PAXWEB-681] - Concurrency bug in ExtenderContext
    Conflicts:
    	pax-web-extender-whiteboard/src/main/java/org/ops4j/pax/web/extender/whiteboard/internal/ExtenderContext.java

--- pax-web-extender-whiteboard/src/main/java/org/ops4j/pax/web/extender/whiteboard/internal/ExtenderContext.java
+++ pax-web-extender-whiteboard/src/main/java/org/ops4j/pax/web/extender/whiteboard/internal/ExtenderContext.java
@@ -55,8 +55,13 @@ public class ExtenderContext {
 		WebApplication webApplication = webApplications.get(contextKey);
 		if (webApplication == null) {
             webApplication = new WebApplication(bundle, httpContextId);
-            if (webApplications.putIfAbsent(contextKey, webApplication) == null) {
+            // PAXWEB-681 - webApplication and existing webApplication might not be the same.
+            WebApplication existingWebApplication = webApplications.putIfAbsent(contextKey, webApplication);
+			if (existingWebApplication == null || !existingWebApplication.equals(webApplication) ) {
                 webApplication.start();
+            } else {
+            	webApplication = existingWebApplication;
+
             }
 		}
 		return webApplication;
