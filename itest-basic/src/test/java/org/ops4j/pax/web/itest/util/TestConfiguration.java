be138e97d5914445bdd78ed409ca9301df958b02
Harald Wellmann <harald.wellmann@gmx.de>
2014-07-20 17:21:58 +0200

    deleted api module

--- itest-basic/src/test/java/org/ops4j/pax/web/itest/util/TestConfiguration.java
+++ itest-basic/src/test/java/org/ops4j/pax/web/itest/util/TestConfiguration.java
@@ -62,7 +62,6 @@ public class TestConfiguration {
             linkBundle("org.apache.felix.eventadmin"),
 
             workspaceBundle("org.ops4j.pax.web", "pax-web-extender"),
-            workspaceBundle("org.ops4j.pax.web", "pax-web-api"),
             workspaceBundle("org.ops4j.pax.web", "pax-web-spi"),
             workspaceBundle("org.ops4j.pax.web", "pax-web-descriptor"),
             workspaceBundle("org.ops4j.pax.web", "pax-web-jaas"),
