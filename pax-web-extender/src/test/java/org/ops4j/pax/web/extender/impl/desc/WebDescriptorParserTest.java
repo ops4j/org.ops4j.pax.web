98d05ae8e0bdf771decc017ebf897272ab633c2a
Harald Wellmann <harald.wellmann@gmx.de>
2014-07-20 17:25:32 +0200

    set Servlet 3.1 XML namespace to make JAXB parse older web.xml versions
    Not required on Oracle JDK 8, but necessary on Oracle JDK 7.

--- pax-web-extender/src/test/java/org/ops4j/pax/web/extender/impl/desc/WebDescriptorParserTest.java
+++ pax-web-extender/src/test/java/org/ops4j/pax/web/extender/impl/desc/WebDescriptorParserTest.java
@@ -1,10 +1,29 @@
+/*
+ * Copyright 2014 Harald Wellmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ops4j.pax.web.extender.impl.desc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-
-import javax.xml.bind.JAXBElement;
 
 import org.junit.Test;
 import org.ops4j.pax.web.descriptor.gen.WebAppType;
@@ -19,8 +38,7 @@ public class WebDescriptorParserTest {
         File file = new File("../samples/pax-web-sample-auth-basic/src/main/webapp/WEB-INF/web.xml");
         URL url = file.getCanonicalFile().toURI().toURL();
         WebAppType descriptor = parser.parseWebXml(url);
-        for (JAXBElement<?> elem :descriptor.getModuleNameOrDescriptionAndDisplayName()) {
-            System.out.println(elem.getValue().getClass());
-        }
+        assertThat(descriptor, is(notNullValue()));
+        assertThat(descriptor.getModuleNameOrDescriptionAndDisplayName().size(), is(6));
     }
 }
