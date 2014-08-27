35e0496bb19e31ae503db750a38ed17d48dd4375
anierbeck <bcanhome@googlemail.com>
2014-08-27 21:37:38 +0200

    manual is now a war

---
+++ pax-web-itest/pax-web-itest-container/pax-web-itest-container-jetty/src/test/java/org/ops4j/pax/web/itest/jetty/WarManualIntegrationTest.java
@@ -0,0 +1,93 @@
+package org.ops4j.pax.web.itest.jetty;
+
+import static org.junit.Assert.fail;
+
+import java.util.Dictionary;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.web.itest.base.VersionUtil;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @author Achim Nierbeck
+ */
+@RunWith(PaxExam.class)
+public class WarManualIntegrationTest extends ITestBase {
+
+	private static final Logger LOG = LoggerFactory.getLogger(WarManualIntegrationTest.class);
+
+	private Bundle installWarBundle;
+
+	@Configuration
+	public static Option[] configure() {
+		return configureJetty();
+	}
+
+
+	@Before
+	public void setUp() throws BundleException, InterruptedException {
+		LOG.info("Setting up test");
+		
+		initWebListener();
+		
+		String bundlePath = WEB_BUNDLE
+				+ "mvn:org.ops4j.pax.web/pax-web-manual/"
+				+ VersionUtil.getProjectVersion() + "/war?"
+				+ WEB_CONTEXT_PATH + "=/pax-web-manual";
+		installWarBundle = bundleContext.installBundle(bundlePath);
+		installWarBundle.start();
+
+		waitForWebListener();
+	}
+
+	@After
+	public void tearDown() throws BundleException {
+		if (installWarBundle != null) {
+			installWarBundle.stop();
+			installWarBundle.uninstall();
+		}
+	}
+
+	/**
+	 * You will get a list of bundles installed by default plus your testcase,
+	 * wrapped into a bundle called pax-exam-probe
+	 */
+	@Test
+	public void listBundles() {
+		for (Bundle b : bundleContext.getBundles()) {
+			if (b.getState() != Bundle.ACTIVE) {
+				fail("Bundle should be active: " + b);
+			}
+
+			Dictionary<String,String> headers = b.getHeaders();
+			String ctxtPath = (String) headers.get(WEB_CONTEXT_PATH);
+			if (ctxtPath != null) {
+				System.out.println("Bundle " + b.getBundleId() + " : "
+						+ b.getSymbolicName() + " : " + ctxtPath);
+			} else {
+				System.out.println("Bundle " + b.getBundleId() + " : "
+						+ b.getSymbolicName());
+			}
+		}
+
+	}
+
+	@Test
+	public void testManual() throws Exception {
+
+		testClient.testWebPath("http://127.0.0.1:8181/pax-web-manual", "<title>Pax Web</title>");
+			
+	}
+
+}
+
