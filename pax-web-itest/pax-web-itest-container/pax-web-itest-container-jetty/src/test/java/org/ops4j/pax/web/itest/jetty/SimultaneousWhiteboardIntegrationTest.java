28968802bf72c7d56f67df0321d998ee02f92d23
anierbeck <bcanhome@googlemail.com>
2015-06-22 20:48:05 +0200

    Revert "[PAXWEB-851] Ignore whiteboard itests as they randomly fail"
    This reverts commit 6ec4dbad1e0f2bc6f88f7fb6804b19f9af4e7b6d.

--- pax-web-itest/pax-web-itest-container/pax-web-itest-container-jetty/src/test/java/org/ops4j/pax/web/itest/jetty/SimultaneousWhiteboardIntegrationTest.java
+++ pax-web-itest/pax-web-itest-container/pax-web-itest-container-jetty/src/test/java/org/ops4j/pax/web/itest/jetty/SimultaneousWhiteboardIntegrationTest.java
@@ -9,7 +9,6 @@ import static org.ops4j.pax.exam.OptionUtils.combine;
 import static org.ops4j.pax.tinybundles.core.TinyBundles.bundle;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
@@ -76,7 +75,6 @@ public class SimultaneousWhiteboardIntegrationTest extends ITestBase {
 	 * You will get a list of bundles installed by default plus your testcase,
 	 * wrapped into a bundle called pax-exam-probe
 	 */
-	@Ignore("PAXWEB-851")
 	@Test
 	public void listBundles() {
 		for (Bundle b : bundleContext.getBundles()) {
@@ -87,13 +85,11 @@ public class SimultaneousWhiteboardIntegrationTest extends ITestBase {
 
 	}
 
-	@Ignore("PAXWEB-851")
 	@Test
 	public void testWhiteBoardRoot() throws Exception {
 		testClient.testWebPath("http://127.0.0.1:8181/root", "Hello Whiteboard Extender");
 	}
 
-	@Ignore("PAXWEB-851")
 	@Test
 	public void testWhiteBoardSlash() throws Exception {
 		testClient.testWebPath("http://127.0.0.1:8181/", "Welcome to the Welcome page");
