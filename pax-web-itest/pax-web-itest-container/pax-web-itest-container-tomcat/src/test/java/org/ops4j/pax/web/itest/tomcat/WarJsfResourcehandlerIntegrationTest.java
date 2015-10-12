18c4520b310645213d68fbf9bde8322cbd64a376
Marc Schlegel <marc.schlegel@gmx.de>
2015-10-12 22:53:05 +0200

    Tomcat-libs not osgi-fied. Use tipi...which currently isnt available via central

--- pax-web-itest/pax-web-itest-container/pax-web-itest-container-tomcat/src/test/java/org/ops4j/pax/web/itest/tomcat/WarJsfResourcehandlerIntegrationTest.java
+++ pax-web-itest/pax-web-itest-container/pax-web-itest-container-tomcat/src/test/java/org/ops4j/pax/web/itest/tomcat/WarJsfResourcehandlerIntegrationTest.java
@@ -56,8 +56,7 @@ public class WarJsfResourcehandlerIntegrationTest extends ITestBase {
     private Option[] configureMyfacesWithSamples() {
         return options(
                 // EL
-                mavenBundle("org.apache.tomcat", "tomcat-el-api").versionAsInProject(),
-                mavenBundle("org.apache.tomcat", "tomcat-jasper-el").versionAsInProject(),
+//                mavenBundle("org.ops4j.pax.tipi", "org.ops4j.pax.tipi.tomcat-embed-el").version("8.0.14.1"),
                 // MyFaces
                 mavenBundle("org.apache.myfaces.core", "myfaces-api").versionAsInProject(),
                 mavenBundle("org.apache.myfaces.core", "myfaces-impl").versionAsInProject(),
