41f0fd50690a215ff061f5ac17b529ee3663ba8d
lostiniceland <marc.schlegel@gmx.de>
2017-01-09 18:09:44 +0100

    [PAXWEB-1090] Integration-Test with Aries-JAXRS-Whiteboard.

---
+++ samples/whiteboard-ds-jaxrs/src/main/java/org/ops4j/pax/web/samples/whiteboard/ds/jaxrs/WhiteboardJaxRsApplication.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.web.samples.whiteboard.ds.jaxrs;
+
+import org.osgi.service.component.annotations.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import java.util.Collections;
+import java.util.Set;
+
+import static org.osgi.service.jaxrs.whiteboard.JaxRSWhiteboardConstants.*;
+
+
+@Component(
+        immediate = true,
+        property = JAX_RS_APPLICATION_BASE + "=/jaxrs-application",
+        service = Application.class
+)
+public class WhiteboardJaxRsApplication extends Application {
+
+    @Override
+    public Set<Object> getSingletons() {
+        return Collections.singleton(this);
+    }
+
+    @GET
+    @Produces("text/plain")
+    public String sayHello() {
+        return "Hello from JAXRS";
+    }
+}
