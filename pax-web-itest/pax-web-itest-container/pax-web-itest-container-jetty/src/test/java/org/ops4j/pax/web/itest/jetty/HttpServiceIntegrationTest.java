4932b5c82b4432deef01ea4a2acd437d7188510f
Stephan Siano <stephan.siano@sap.com>
2017-11-28 13:20:15 +0100

    [PAXWEB-1083] clean up integration tests
    - Have the pax-web-itest-common bundle to dynamically import everything
    - Clean up HttpServiceIntegrationTest

--- pax-web-itest/pax-web-itest-container/pax-web-itest-container-jetty/src/test/java/org/ops4j/pax/web/itest/jetty/HttpServiceIntegrationTest.java
+++ pax-web-itest/pax-web-itest-container/pax-web-itest-container-jetty/src/test/java/org/ops4j/pax/web/itest/jetty/HttpServiceIntegrationTest.java
@@ -15,13 +15,28 @@
  */
 package org.ops4j.pax.web.itest.jetty;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.ops4j.pax.web.itest.base.WaitCondition;
 import org.ops4j.pax.web.itest.common.AbstractHttpServiceIntegrationTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Toni Menzel (tonit)
@@ -30,6 +45,7 @@ import org.ops4j.pax.web.itest.common.AbstractHttpServiceIntegrationTest;
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerMethod.class)
 public class HttpServiceIntegrationTest extends AbstractHttpServiceIntegrationTest {
+    private static final Logger LOG = LoggerFactory.getLogger(HttpServiceIntegrationTest.class);
 
 	@Configuration
 	public static Option[] configure() {
@@ -39,5 +55,39 @@ public class HttpServiceIntegrationTest extends AbstractHttpServiceIntegrationTe
 	@Override
     protected boolean isInitEager() {
         return true;
+    }
+
+    @Test
+    // Logging depends on the container implementation and configuration
+    public void testNCSALogger() throws Exception {
+        testServletPath();
+
+        SimpleDateFormat formater = new SimpleDateFormat("yyyy_MM_dd");
+        String date = formater.format(new Date());
+
+        final File logFile = new File("target/logs/" + date + ".request.log");
+
+        LOG.info("Log-File: {}", logFile.getAbsoluteFile());
+
+        assertNotNull(logFile);
+
+        new WaitCondition("logfile") {
+            @Override
+            protected boolean isFulfilled() throws Exception {
+                return logFile.exists();
+            }
+        }.waitForCondition();
+
+        boolean exists = logFile.getAbsoluteFile().exists();
+
+        assertTrue(exists);
+
+        FileInputStream fstream = new FileInputStream(logFile.getAbsoluteFile());
+        DataInputStream in = new DataInputStream(fstream);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String strLine = br.readLine();
+        assertNotNull(strLine);
+        in.close();
+        fstream.close();
     }
 }
