7b0142a293e6899e351db91465964a64fa78947b
Stephan Siano <stephan.siano@sap.com>
2017-12-07 11:10:03 +0100

    [PAXWEB-1139] ServletContextListener.contextDestroyed() is not called

--- pax-web-tomcat/src/main/java/org/ops4j/pax/web/service/tomcat/internal/TomcatServerWrapper.java
+++ pax-web-tomcat/src/main/java/org/ops4j/pax/web/service/tomcat/internal/TomcatServerWrapper.java
@@ -41,6 +41,7 @@ import javax.servlet.FilterRegistration.Dynamic;
 import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextAttributeListener;
+import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -635,6 +636,10 @@ class TomcatServerWrapper implements ServerWrapper {
 		boolean found = filterEventListener(listeners, applicationLifecycleListeners, eventListener);
 
 		if (found) {
+			// notify the ServletContextListener before unregistering it
+			if (eventListener instanceof ServletContextListener) {
+				((ServletContextListener) eventListener).contextDestroyed(new ServletContextEvent(context.getServletContext()));
+			}
 			context.setApplicationLifecycleListeners(listeners.toArray());
 		}
 		return found;
