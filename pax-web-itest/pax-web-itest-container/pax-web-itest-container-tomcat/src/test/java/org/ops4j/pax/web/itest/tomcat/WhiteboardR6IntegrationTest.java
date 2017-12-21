c409b98f4478e2d6e13992d38962da50a65a7693
Stephan Siano <stephan.siano@sap.com>
2017-12-21 16:00:11 +0100

    [PAXWEB-1142] Prepare integration test to demonstrate issue

--- pax-web-itest/pax-web-itest-container/pax-web-itest-container-tomcat/src/test/java/org/ops4j/pax/web/itest/tomcat/WhiteboardR6IntegrationTest.java
+++ pax-web-itest/pax-web-itest-container/pax-web-itest-container-tomcat/src/test/java/org/ops4j/pax/web/itest/tomcat/WhiteboardR6IntegrationTest.java
@@ -15,25 +15,54 @@
  */
 package org.ops4j.pax.web.itest.tomcat;
 
-import org.junit.Ignore;
-import org.junit.Test;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.web.itest.common.AbstractWhiteboardR6IntegrationTest;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 
 @RunWith(PaxExam.class)
 public class WhiteboardR6IntegrationTest extends AbstractWhiteboardR6IntegrationTest {
+
+    private static final Map<Integer, String> ERROR_MESSAGES = Stream.of(
+            new SimpleEntry<>(404, ""),
+            new SimpleEntry<>(442, ""),
+            new SimpleEntry<>(502, ""),
+            new SimpleEntry<>(500, "somethingwronghashappened"))
+            .collect(Collectors.toMap(SimpleEntry::getKey, SimpleEntry::getValue));
 
 	@Configuration
 	public static Option[] configure() {
 		return configureTomcat();
 	}
 
-	@Test
-	@Ignore
-	public void testErrorServlet() throws Exception {
-		super.testErrorServlet();
+    @Override
+    protected String getErrorMessage(int statusCode) {
+        return ERROR_MESSAGES.get(statusCode);
+    }
+
+    @Override
+    /*
+     * This is a workaround for PAXWEB-1142
+     *
+     * The error page registration should actually be for "5xx" instead of "500", "502"
+     *
+     */
+	protected Dictionary<String, Object> getRegistrationProperties() {
+		Dictionary<String, Object> properties = new Hashtable<>();
+		properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_ERROR_PAGE, new String[] {
+				"404", "442", "500", "502",
+				"java.io.IOException"
+		});
+		return properties;
 	}
+
 }
