347fc9d0edb0b880dccf10617f7dd3cd26f887e1
Stephan Siano <stephan.siano@sap.com>
2018-03-21 07:35:49 +0100

    [PAXWEB-1156] fix integration tests

--- pax-web-itest/pax-web-itest-container/pax-web-itest-container-undertow/src/test/java/org/ops4j/pax/web/itest/undertow/WarExtendedIntegrationTest.java
+++ pax-web-itest/pax-web-itest-container/pax-web-itest-container-undertow/src/test/java/org/ops4j/pax/web/itest/undertow/WarExtendedIntegrationTest.java
@@ -123,12 +123,12 @@ public class WarExtendedIntegrationTest extends ITestBase {
 
 		assertThat("There should be single ServletContext disposal",
 				events1.stream().filter(s -> s.equals("contextDestroyed: /war1")).count(), equalTo(1L));
-		assertThat("war1 should have 5 registration and 5 unregistration events recorded",
-				events1.size(), equalTo(10));
+		assertThat("war1 should have 5 registration, 5 unregistration, and 1 parameter found events recorded",
+				events1.size(), equalTo(11));
 		assertTrue("There should be no ServletContext disposal",
 				events2.stream().noneMatch(s -> s.equals("contextDestroyed: /war2")));
-		assertThat("war2 should have only 5 registration events recorded",
-				events2.size(), equalTo(5));
+		assertThat("war2 should have only 5 registration and 1 parameter found events recorded",
+				events2.size(), equalTo(6));
 
 		HttpTestClientFactory.createDefaultTestClient()
 				.withReturnCode(HttpServletResponse.SC_NOT_FOUND)
