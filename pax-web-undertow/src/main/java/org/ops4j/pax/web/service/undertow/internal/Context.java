6f2ab9090fa88cd8a97a3818dda71d480b31e6c0
Grzegorz Grzybek <gr.grzybek@gmail.com>
2018-07-31 15:52:33 +0200

    [PAXWEB-1173] Shutdown all XNIO workers created by pax-web-undertow - when needed

--- pax-web-undertow/src/main/java/org/ops4j/pax/web/service/undertow/internal/Context.java
+++ pax-web-undertow/src/main/java/org/ops4j/pax/web/service/undertow/internal/Context.java
@@ -121,6 +121,7 @@ public class Context implements LifeCycle, HttpHandler, ResourceManager {
 	private ServiceTracker<PackageAdmin, PackageAdmin> packageAdminTracker;
 
 	private Configuration configuration;
+	private XnioWorker wsXnioWorker;
 
 	public Context(IdentityManager identityManager, ContextAwarePathHandler path, ContextModel contextModel) {
 		this.identityManager = identityManager;
@@ -229,6 +230,10 @@ public class Context implements LifeCycle, HttpHandler, ResourceManager {
 		} catch (ServletException e) {
 			LOG.error(e.getMessage(), e);
 		}
+		if (wsXnioWorker != null) {
+			wsXnioWorker.shutdown();
+		}
+
 		// clean up.
 		// we really have to unregister all remaining ServletContext registrations, implicit Default Servlets
 		// and even normal servlets. For example CXF re-registers CXFServlet even when stopping
@@ -583,12 +588,12 @@ public class Context implements LifeCycle, HttpHandler, ResourceManager {
 		}
 
 		if (isWebSocketAvailable()) {
-			XnioWorker xnioWorker = UndertowUtil.createWorker(contextModel.getClassLoader());
-			if (xnioWorker != null) {
+			wsXnioWorker = UndertowUtil.createWorker(contextModel.getClassLoader());
+			if (wsXnioWorker != null) {
 				deployment.addServletContextAttribute(
 						io.undertow.websockets.jsr.WebSocketDeploymentInfo.ATTRIBUTE_NAME,
 						new io.undertow.websockets.jsr.WebSocketDeploymentInfo()
-								.setWorker(xnioWorker)
+								.setWorker(wsXnioWorker)
 								.setBuffers(new DefaultByteBufferPool(true, 100))
 				);
 			}
--- pax-web-undertow/src/main/java/org/ops4j/pax/web/service/undertow/internal/ServerControllerImpl.java
+++ pax-web-undertow/src/main/java/org/ops4j/pax/web/service/undertow/internal/ServerControllerImpl.java
@@ -144,6 +144,8 @@ public class ServerControllerImpl implements ServerController, ServerControllerE
     private Undertow server;
     private final ConcurrentMap<HttpContext, Context> contextMap = new ConcurrentHashMap<>();
 
+    private XnioWorker xnioWorker;
+
     public ServerControllerImpl(BundleContext context) {
         this.bundleContext = context;
     }
@@ -321,12 +323,12 @@ public class ServerControllerImpl implements ServerController, ServerControllerE
 
             Bundle bundle = FrameworkUtil.getBundle(ServerControllerImpl.class);
             ClassLoader loader = bundle.adapt(BundleWiring.class).getClassLoader();
-            XnioWorker worker = UndertowUtil.createWorker(loader);
+            xnioWorker = UndertowUtil.createWorker(loader);
 
             // String logNameSuffix = logNCSAFormat.substring(logNCSAFormat.lastIndexOf("."));
             // String logBaseName = logNCSAFormat.substring(0, logNCSAFormat.lastIndexOf("."));
 
-            AccessLogReceiver logReceiver = DefaultAccessLogReceiver.builder().setLogWriteExecutor(worker)
+            AccessLogReceiver logReceiver = DefaultAccessLogReceiver.builder().setLogWriteExecutor(xnioWorker)
                     .setOutputDirectory(new File(logNCSADirectory).toPath()).setLogBaseName("request.")
                     .setLogNameSuffix("log").setRotate(true).build();
 
@@ -565,10 +567,10 @@ public class ServerControllerImpl implements ServerController, ServerControllerE
 
                 Bundle bundle = FrameworkUtil.getBundle(ServerControllerImpl.class);
                 ClassLoader loader = bundle.adapt(BundleWiring.class).getClassLoader();
-                XnioWorker worker = UndertowUtil.createWorker(loader);
+                xnioWorker = UndertowUtil.createWorker(loader);
 
                 AccessLogReceiver logReceiver = DefaultAccessLogReceiver.builder()
-                        .setLogWriteExecutor(worker)
+                        .setLogWriteExecutor(xnioWorker)
                         .setOutputDirectory(new File(accessLog.getDirectory()).toPath())
                         .setLogBaseName(accessLog.getPrefix())
                         .setLogNameSuffix(accessLog.getSuffix())
@@ -848,6 +850,9 @@ public class ServerControllerImpl implements ServerController, ServerControllerE
     }
 
     void doStop() {
+        if (xnioWorker != null) {
+            xnioWorker.shutdown();
+        }
         server.stop();
     }
 
