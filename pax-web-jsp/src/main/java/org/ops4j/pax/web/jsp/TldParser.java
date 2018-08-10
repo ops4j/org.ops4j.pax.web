b999c729e11d7296dc87bcba503e3d38f73dce09
Stephan Siano <stephan.siano@sap.com>
2018-08-10 11:48:37 +0200

    [PAXWEB-1176] Update to Tomcat 8.5.32

--- pax-web-jsp/src/main/java/org/ops4j/pax/web/jsp/TldParser.java
+++ pax-web-jsp/src/main/java/org/ops4j/pax/web/jsp/TldParser.java
@@ -38,69 +38,64 @@ import org.xml.sax.SAXException;
  * Parses a Tag Library Descriptor.
  */
 public class TldParser {
-	private static final Log LOG = LogFactory.getLog(TldParser.class);
-	private final Digester digester;
+    private final Log log = LogFactory.getLog(TldParser.class); // must not be static
+    private final Digester digester;
 
-	public TldParser(boolean namespaceAware, boolean validation,
-					 boolean blockExternal) {
-		this(namespaceAware, validation, new TldRuleSet(), blockExternal);
-	}
+    public TldParser(boolean namespaceAware, boolean validation,
+            boolean blockExternal) {
+        this(namespaceAware, validation, new TldRuleSet(), blockExternal);
+    }
 
-	public TldParser(boolean namespaceAware, boolean validation,
-					 RuleSet ruleSet, boolean blockExternal) {
-		digester = DigesterFactory.newDigester(validation, namespaceAware,
-				ruleSet, blockExternal);
-	}
+    public TldParser(boolean namespaceAware, boolean validation, RuleSet ruleSet,
+            boolean blockExternal) {
+        digester = DigesterFactory.newDigester(
+                validation, namespaceAware, ruleSet, blockExternal);
+    }
 
-	public TaglibXml parse(TldResourcePath path) throws IOException,
-			SAXException {
-		ClassLoader original;
-		if (Constants.IS_SECURITY_ENABLED) {
-			PrivilegedGetTccl pa = new PrivilegedGetTccl();
-			original = AccessController.doPrivileged(pa);
-		} else {
-			original = Thread.currentThread().getContextClassLoader();
-		}
-		try (InputStream is = path.openStream()) {
-			if (Constants.IS_SECURITY_ENABLED) {
-				PrivilegedSetTccl pa = new PrivilegedSetTccl(
-						TldParser.class.getClassLoader());
-				AccessController.doPrivileged(pa);
-			} else {
-				Thread.currentThread().setContextClassLoader(
-						TldParser.class.getClassLoader());
-			}
-			XmlErrorHandler handler = new XmlErrorHandler();
-			digester.setErrorHandler(handler);
+    public TaglibXml parse(TldResourcePath path) throws IOException, SAXException {
+        ClassLoader original;
+        if (Constants.IS_SECURITY_ENABLED) {
+            PrivilegedGetTccl pa = new PrivilegedGetTccl();
+            original = AccessController.doPrivileged(pa);
+        } else {
+            original = Thread.currentThread().getContextClassLoader();
+        }
+        try (InputStream is = path.openStream()) {
+            if (Constants.IS_SECURITY_ENABLED) {
+                PrivilegedSetTccl pa = new PrivilegedSetTccl(TldParser.class.getClassLoader());
+                AccessController.doPrivileged(pa);
+            } else {
+                Thread.currentThread().setContextClassLoader(TldParser.class.getClassLoader());
+            }
+            XmlErrorHandler handler = new XmlErrorHandler();
+            digester.setErrorHandler(handler);
 
-			TaglibXml taglibXml = new TaglibXml();
-			digester.push(taglibXml);
+            TaglibXml taglibXml = new TaglibXml();
+            digester.push(taglibXml);
 
-			InputSource source = new InputSource(path.toExternalForm());
-			source.setByteStream(is);
-			digester.parse(source);
-			if (!handler.getWarnings().isEmpty()
-					|| !handler.getErrors().isEmpty()) {
-				handler.logFindings(LOG, source.getSystemId());
-				if (!handler.getErrors().isEmpty()) {
-					// throw the first to indicate there was a error during
-					// processing
-					throw handler.getErrors().iterator().next();
-				}
-			}
-			return taglibXml;
-		} finally {
-			digester.reset();
-			if (Constants.IS_SECURITY_ENABLED) {
-				PrivilegedSetTccl pa = new PrivilegedSetTccl(original);
-				AccessController.doPrivileged(pa);
-			} else {
-				Thread.currentThread().setContextClassLoader(original);
-			}
-		}
-	}
+            InputSource source = new InputSource(path.toExternalForm());
+            source.setByteStream(is);
+            digester.parse(source);
+            if (!handler.getWarnings().isEmpty() || !handler.getErrors().isEmpty()) {
+                handler.logFindings(log, source.getSystemId());
+                if (!handler.getErrors().isEmpty()) {
+                    // throw the first to indicate there was a error during processing
+                    throw handler.getErrors().iterator().next();
+                }
+            }
+            return taglibXml;
+        } finally {
+            digester.reset();
+            if (Constants.IS_SECURITY_ENABLED) {
+                PrivilegedSetTccl pa = new PrivilegedSetTccl(original);
+                AccessController.doPrivileged(pa);
+            } else {
+                Thread.currentThread().setContextClassLoader(original);
+            }
+        }
+    }
 
-	public void setClassLoader(ClassLoader classLoader) {
-		digester.setClassLoader(classLoader);
-	}
+    public void setClassLoader(ClassLoader classLoader) {
+        digester.setClassLoader(classLoader);
+    }
 }
