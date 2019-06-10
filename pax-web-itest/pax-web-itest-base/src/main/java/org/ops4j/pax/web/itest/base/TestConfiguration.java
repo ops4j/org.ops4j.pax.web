357242dc09741b50a2b838a2d4458b9971bc9eee
Jean-Baptiste Onofré <jbonofre@apache.org>
2019-06-10 16:17:30 +0200

    [PAXWEB-1207] Upgrade to commons-beanutils 1.9.3

--- pax-web-itest/pax-web-itest-base/src/main/java/org/ops4j/pax/web/itest/base/TestConfiguration.java
+++ pax-web-itest/pax-web-itest-base/src/main/java/org/ops4j/pax/web/itest/base/TestConfiguration.java
@@ -120,7 +120,7 @@ public class TestConfiguration {
 				// Commons
 				mavenBundle("commons-io", "commons-io").version("1.4"),
 				mavenBundle("commons-codec", "commons-codec").version("1.10"),
-				mavenBundle("commons-beanutils", "commons-beanutils").version("1.8.3"),
+				mavenBundle("commons-beanutils", "commons-beanutils").version("1.9.3"),
 				mavenBundle("commons-collections", "commons-collections").version("3.2.1"),
 				mavenBundle("commons-digester", "commons-digester").version("1.8.1"),
 				mavenBundle("org.apache.commons", "commons-lang3").version("3.4")
