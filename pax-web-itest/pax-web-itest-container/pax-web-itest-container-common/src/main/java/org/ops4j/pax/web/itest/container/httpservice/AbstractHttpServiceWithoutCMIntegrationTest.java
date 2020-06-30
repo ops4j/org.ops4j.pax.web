b59397e772dd69f7a1acf4d3b02f11d00a3946f2
Grzegorz Grzybek <gr.grzybek@gmail.com>
2020-06-30 19:12:56 +0200

    [test] HttpServiceWithoutCMIntegrationTest tests work

--- pax-web-itest/pax-web-itest-container/pax-web-itest-container-common/src/main/java/org/ops4j/pax/web/itest/container/httpservice/AbstractHttpServiceWithoutCMIntegrationTest.java
+++ pax-web-itest/pax-web-itest-container/pax-web-itest-container-common/src/main/java/org/ops4j/pax/web/itest/container/httpservice/AbstractHttpServiceWithoutCMIntegrationTest.java
@@ -15,9 +15,14 @@
  */
 package org.ops4j.pax.web.itest.container.httpservice;
 
-import org.ops4j.pax.web.itest.container.AbstractControlledTestBase;
-import org.osgi.framework.Bundle;
+import javax.servlet.http.HttpServletResponse;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.ops4j.pax.web.itest.container.AbstractControlledTestBase;
+import org.ops4j.pax.web.itest.utils.client.HttpTestClientFactory;
+import org.osgi.framework.Bundle;
 
 /**
  * @author Toni Menzel (tonit)
@@ -25,57 +30,59 @@ import org.osgi.framework.Bundle;
  */
 public abstract class AbstractHttpServiceWithoutCMIntegrationTest extends AbstractControlledTestBase {
 
-	private Bundle installWarBundle;
+	private Bundle hsBundle;
 
-//	@Before
-//	public void setUp() throws BundleException, InterruptedException {
-//		initWebListener();
-//		String bundlePath = "mvn:org.ops4j.pax.web.samples/helloworld-hs/" + VersionUtil.getProjectVersion();
-//		installWarBundle = installAndStartBundle(bundlePath);
-//		waitForWebListener();
-//	}
-//
-//	@After
-//	public void tearDown() throws BundleException {
-//		if (installWarBundle != null) {
-//			installWarBundle.stop();
-//			installWarBundle.uninstall();
-//		}
-//	}
-//
-//
-//	@Test
-//	public void testSubPath() throws Exception {
-//		HttpTestClientFactory.createDefaultTestClient()
-//				.withResponseAssertion("Response must contain 'Hello World'",
-//						resp -> resp.contains("Hello World"))
-//				.doGETandExecuteTest("http://127.0.0.1:8181/helloworld/hs");
-//		// test image-serving
-//		HttpTestClientFactory.createDefaultTestClient()
-//				.doGETandExecuteTest("http://127.0.0.1:8181/images/logo.png");
-//	}
-//
-//	@Test
-//	public void testRootPath() throws Exception {
-//		HttpTestClientFactory.createDefaultTestClient()
-//				.doGETandExecuteTest("http://127.0.0.1:8181/");
-//	}
-//
-//	@Test
-//	public void testServletPath() throws Exception {
-//		HttpTestClientFactory.createDefaultTestClient()
-//				.withResponseAssertion("Response must contain 'Servlet Path: '",
-//						resp -> resp.contains("Servlet Path: "))
-//				.withResponseAssertion("Response must contain 'Path Info: /lall/blubb'",
-//						resp -> resp.contains("Path Info: /lall/blubb"))
-//				.doGETandExecuteTest("http://127.0.0.1:8181/lall/blubb");
-//	}
-//
-//	@Test
-//	public void testServletDeRegistration() throws Exception {
-//
-//		if (installWarBundle != null) {
-//			installWarBundle.stop();
-//		}
-//	}
+	@Before
+	public void setUp() throws Exception {
+		configureAndWaitForServletWithMapping("/alt-images/*",
+				() -> hsBundle = installAndStartBundle(sampleURI("hs-helloworld")));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		if (hsBundle != null) {
+			hsBundle.stop();
+			hsBundle.uninstall();
+		}
+	}
+
+	@Test
+	public void testSubPath() throws Exception {
+		HttpTestClientFactory.createDefaultTestClient()
+				.withResponseAssertion("Response must contain 'Hello World'",
+						resp -> resp.contains("Hello World"))
+				.doGETandExecuteTest("http://127.0.0.1:8181/helloworld/hs");
+		// test image-serving
+		HttpTestClientFactory.createDefaultTestClient()
+				.doGETandExecuteTest("http://127.0.0.1:8181/images/logo.png");
+	}
+
+	@Test
+	public void testRootPath() throws Exception {
+		HttpTestClientFactory.createDefaultTestClient()
+				.withReturnCode(HttpServletResponse.SC_OK)
+				.doGETandExecuteTest("http://127.0.0.1:8181/");
+	}
+
+	@Test
+	public void testServletPath() throws Exception {
+		HttpTestClientFactory.createDefaultTestClient()
+				.withResponseAssertion("Response must contain 'Servlet Path: \"\"'",
+						resp -> resp.contains("Servlet Path: "))
+				.withResponseAssertion("Response must contain 'Path Info: \"/lall/blubb\"'",
+						resp -> resp.contains("Path Info: \"/lall/blubb\""))
+				.doGETandExecuteTest("http://127.0.0.1:8181/lall/blubb");
+	}
+
+	@Test
+	public void testServletDeRegistration() throws Exception {
+		if (hsBundle != null) {
+			hsBundle.stop();
+		}
+
+		HttpTestClientFactory.createDefaultTestClient()
+				.withReturnCode(HttpServletResponse.SC_NOT_FOUND)
+				.doGETandExecuteTest("http://127.0.0.1:8181/");
+	}
+
 }
