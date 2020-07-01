4a0b512dde2bd4640c7c614aa4d64af33ac982c9
Stephan Siano <stephan.siano@sap.com>
2020-07-01 16:25:26 +0200

    [PAXWEB-1260] Update Tomcat version to 9.0.36

--- pax-web-jsp/src/main/java/org/apache/jasper/JspCompilationContext.java
+++ pax-web-jsp/src/main/java/org/apache/jasper/JspCompilationContext.java
@@ -85,7 +85,9 @@ public class JspCompilationContext {
 
     private volatile boolean removed = false;
 
-    private URLClassLoader jspLoader;
+    // volatile so changes are visible when multiple threads request a JSP file
+    // that has been modified
+    private volatile URLClassLoader jspLoader;
     private URL baseUrl;
     private Class<?> servletClass;
 
@@ -465,11 +467,11 @@ public class JspCompilationContext {
         if (isTagFile()) {
             String className = tagInfo.getTagClassName();
             int lastIndex = className.lastIndexOf('.');
-            String pkgName = "";
+            String packageName = "";
             if (lastIndex != -1) {
-                pkgName = className.substring(0, lastIndex);
+                packageName = className.substring(0, lastIndex);
             }
-            return pkgName;
+            return packageName;
         } else {
             String dPackageName = getDerivedPackageName();
             if (dPackageName.length() == 0) {
