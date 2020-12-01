7bfffe0f49dc58b9326c2ef3658e02d4bc6d6d82
Grzegorz Grzybek <gr.grzybek@gmail.com>
2020-12-01 12:28:45 +0100

    [tests] WhiteboardResourceIntegrationTest test works

--- pax-web-itest/pax-web-itest-container/pax-web-itest-container-common/src/main/java/org/ops4j/pax/web/itest/container/whiteboard/AbstractWhiteboardResourceIntegrationTest.java
+++ pax-web-itest/pax-web-itest-container/pax-web-itest-container-common/src/main/java/org/ops4j/pax/web/itest/container/whiteboard/AbstractWhiteboardResourceIntegrationTest.java
@@ -15,7 +15,15 @@
  */
 package org.ops4j.pax.web.itest.container.whiteboard;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.ops4j.pax.web.extender.whiteboard.runtime.DefaultResourceMapping;
 import org.ops4j.pax.web.itest.container.AbstractContainerTestBase;
+import org.ops4j.pax.web.itest.utils.client.HttpTestClientFactory;
+import org.ops4j.pax.web.service.whiteboard.ResourceMapping;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.ServiceRegistration;
 
 /**
  * @author Toni Menzel (tonit)
@@ -23,32 +31,30 @@ import org.ops4j.pax.web.itest.container.AbstractContainerTestBase;
  */
 public abstract class AbstractWhiteboardResourceIntegrationTest extends AbstractContainerTestBase {
 
-//	private ServiceRegistration<ResourceMapping> service;
-//
-//	@Before
-//	public void setUp() throws BundleException, InterruptedException {
-//		DefaultResourceMapping resourceMapping = new DefaultResourceMapping();
-//		resourceMapping.setAlias("/whiteboardresources");
-//		resourceMapping.setPath("/images");
-//		service = bundleContext.registerService(ResourceMapping.class,
-//				resourceMapping, null);
-//
-//		initServletListener();
-//		waitForServletListener();
-//	}
-//
-//	@After
-//	public void tearDown() throws BundleException {
-//		service.unregister();
-//	}
-//
-//	@Test
-//	public void testWhiteBoardFiltered() throws Exception {
-//		HttpTestClientFactory.createDefaultTestClient()
-//				.withResponseHeaderAssertion("Header 'Content-Type' must be 'image/png'",
-//						headers -> headers.anyMatch(header -> header.getKey().equals("Content-Type")
-//								&& header.getValue().equals("image/png")))
-//				.doGETandExecuteTest("http://127.0.0.1:8181/whiteboardresources/ops4j.png");
-//	}
+	private ServiceRegistration<ResourceMapping> service;
+
+	@Before
+	public void setUp() throws Exception {
+		configureAndWaitForServletWithMapping("/whiteboardresources/*", () -> {
+			DefaultResourceMapping resourceMapping = new DefaultResourceMapping();
+			resourceMapping.setAlias("/whiteboardresources");
+			resourceMapping.setPath("/images");
+			service = context.registerService(ResourceMapping.class, resourceMapping, null);
+		});
+	}
+
+	@After
+	public void tearDown() throws BundleException {
+		service.unregister();
+	}
+
+	@Test
+	public void testWhiteBoardFiltered() throws Exception {
+		HttpTestClientFactory.createDefaultTestClient()
+				.withResponseHeaderAssertion("Header 'Content-Type' must be 'image/png'",
+						headers -> headers.anyMatch(header -> header.getKey().equals("Content-Type")
+								&& header.getValue().equals("image/png")))
+				.doGETandExecuteTest("http://127.0.0.1:8181/whiteboardresources/ops4j.png");
+	}
 
 }
