c516b253534b1480b63ab8f74031b4848efe81c3
Grzegorz Grzybek <gr.grzybek@gmail.com>
2020-12-01 15:08:34 +0100

    [tests] WebConsoleIntegrationTest test works

---
+++ pax-web-itest/pax-web-itest-container/pax-web-itest-undertow/src/test/java/org/ops4j/pax/web/itest/undertow/httpservice/WebConsoleIntegrationTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2020 OPS4J.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.web.itest.undertow.httpservice;
+
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.web.itest.container.httpservice.AbstractWebConsoleIntegrationTest;
+
+import static org.ops4j.pax.exam.Constants.START_LEVEL_TEST_BUNDLE;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+
+@RunWith(PaxExam.class)
+public class WebConsoleIntegrationTest extends AbstractWebConsoleIntegrationTest {
+
+	@Configuration
+	public Option[] configure() {
+		return combine(baseConfigure(),
+				combine(paxWebUndertow(),
+						// felix.webconsole needs servlet-api 3
+						mavenBundle("org.ops4j.pax.web", "pax-web-jetty-servlet-compatibility")
+								.versionAsInProject().startLevel(START_LEVEL_TEST_BUNDLE - 1).noStart()
+				)
+		);
+	}
+
+}
