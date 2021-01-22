2978260a17fd5bb7fbf2d937a984fbe6f53eddfc
Grzegorz Grzybek <gr.grzybek@gmail.com>
2021-01-22 09:32:47 +0100

    [Whiteboard] Add test for Whiteboard registration of context-bound resources

--- pax-web-itest/pax-web-itest-server/src/test/java/org/ops4j/pax/web/itest/server/whiteboard/WhiteboardResourcesTest.java
+++ pax-web-itest/pax-web-itest-server/src/test/java/org/ops4j/pax/web/itest/server/whiteboard/WhiteboardResourcesTest.java
@@ -30,6 +30,7 @@ import org.ops4j.pax.web.service.spi.model.elements.ServletModel;
 import org.ops4j.pax.web.service.whiteboard.ResourceMapping;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.context.ServletContextHelper;
 import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 
 import static org.hamcrest.CoreMatchers.endsWith;
@@ -86,6 +87,71 @@ public class WhiteboardResourcesTest extends MultiContainerTestSupport {
 		getResourceMappingCustomizer().removedService(resourceMappingRef, model2);
 		assertThat(httpGET(port, "/resources/file.txt"), startsWith("HTTP/1.1 404"));
 		assertThat(httpGET(port, "/resources/other.txt"), startsWith("HTTP/1.1 404"));
+
+		ServerModelInternals serverModelInternals = serverModelInternals(serverModel);
+		ServiceModelInternals serviceModelInternals = serviceModelInternals(sample1);
+
+		assertTrue(serverModelInternals.isClean(whiteboardBundle));
+		assertTrue(serverModelInternals.isClean(sample1));
+		assertTrue(serviceModelInternals.isEmpty());
+	}
+
+	@Test
+	public void twoWaysToRegisterResourcesInCustomContext() throws Exception {
+		File base = new File("target/www");
+		FileUtils.deleteDirectory(base);
+		base.mkdirs();
+		try (FileWriter fw = new FileWriter(new File(base, "file.txt"))) {
+			IOUtils.write("hello1", fw);
+		}
+
+		Bundle sample1 = mockBundle("sample1");
+		when(sample1.getEntry("resources/file.txt")).thenReturn(new File(base, "file.txt").toURI().toURL());
+
+		ServletContextHelper helper = new ServletContextHelper(sample1) {
+		};
+		getServletContextHelperCustomizer().addingService(mockServletContextHelperReference(sample1, "c1",
+				() -> helper, 0L, 0, "/c"));
+
+		// 1. Whiteboard registration as an OSGi service of any objectClass
+
+		Hashtable<String, Object> properties = new Hashtable<>();
+		// OSGi CMPN Whiteboard properties
+		properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_RESOURCE_PATTERN, new String[] { "/files/*", "/res/*" });
+		properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_RESOURCE_PREFIX, "/resources");
+		properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_SELECT, String.format("(%s=c1)",
+				HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME));
+		ServiceReference<Object> resourcesRef = mockReference(sample1, Object.class, properties, Object::new, 0L, 0);
+		ServletModel model = getResourceCustomizer().addingService(resourcesRef);
+		assertThat(httpGET(port, "/files/file.txt"), startsWith("HTTP/1.1 404"));
+		assertThat(httpGET(port, "/res/file.txt"), startsWith("HTTP/1.1 404"));
+		assertThat(httpGET(port, "/c/files/file.txt"), endsWith("hello1"));
+		assertThat(httpGET(port, "/c/res/file.txt"), endsWith("hello1"));
+		assertThat(httpGET(port, "/c/files/other.txt"), startsWith("HTTP/1.1 404"));
+
+		getResourceCustomizer().removedService(resourcesRef, model);
+		assertThat(httpGET(port, "/c/files/file.txt"), startsWith("HTTP/1.1 404"));
+		assertThat(httpGET(port, "/c/files/other.txt"), startsWith("HTTP/1.1 404"));
+
+		// 2. Whiteboard registration as Pax Web specific org.ops4j.pax.web.service.whiteboard.ResourceMapping
+		//    OSGi service
+
+		DefaultResourceMapping rm = new DefaultResourceMapping();
+		rm.setUrlPatterns(new String[] { "/resources/*", "/x/*" });
+		rm.setPath("/resources");
+		rm.setContextSelectFilter(String.format("(%s=c1)", HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME));
+		ServiceReference<ResourceMapping> resourceMappingRef = mockReference(sample1, ResourceMapping.class,
+				null, () -> rm);
+		ServletModel model2 = getResourceMappingCustomizer().addingService(resourceMappingRef);
+		assertThat(httpGET(port, "/resources/file.txt"), startsWith("HTTP/1.1 404"));
+		assertThat(httpGET(port, "/x/file.txt"), startsWith("HTTP/1.1 404"));
+		assertThat(httpGET(port, "/c/resources/file.txt"), endsWith("hello1"));
+		assertThat(httpGET(port, "/c/x/file.txt"), endsWith("hello1"));
+		assertThat(httpGET(port, "/resources/other.txt"), startsWith("HTTP/1.1 404"));
+
+		getResourceMappingCustomizer().removedService(resourceMappingRef, model2);
+		assertThat(httpGET(port, "/c/resources/file.txt"), startsWith("HTTP/1.1 404"));
+		assertThat(httpGET(port, "/c/resources/other.txt"), startsWith("HTTP/1.1 404"));
 
 		ServerModelInternals serverModelInternals = serverModelInternals(serverModel);
 		ServiceModelInternals serviceModelInternals = serviceModelInternals(sample1);
