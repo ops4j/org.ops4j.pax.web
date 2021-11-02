a7ad4203df3d8a35aa2894c63993af5fac243d8c
Grzegorz Grzybek <gr.grzybek@gmail.com>
2021-11-02 09:15:14 +0100

    [#1637] Add pax-web-tomcat-bundle and related integration tests

--- pax-web-tomcat-bundle/src/main/java/org/ops4j/pax/web/service/tomcat/bundle/internal/CompositeActivator.java
+++ pax-web-tomcat-bundle/src/main/java/org/ops4j/pax/web/service/tomcat/bundle/internal/CompositeActivator.java
@@ -5,17 +5,15 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied.
- *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.ops4j.pax.web.service.tomcat.internal;
+package org.ops4j.pax.web.service.tomcat.bundle.internal;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -26,12 +24,12 @@ import org.osgi.framework.BundleContext;
  */
 public class CompositeActivator implements BundleActivator {
 
-	private BundleActivator paxWebActivator;
-	private BundleActivator tomcatActivator;
+	private final BundleActivator paxWebActivator;
+	private final BundleActivator tomcatActivator;
 
 	public CompositeActivator() {
 		paxWebActivator = new org.ops4j.pax.web.service.internal.Activator();
-		tomcatActivator = new Activator();
+		tomcatActivator = new org.ops4j.pax.web.service.tomcat.internal.Activator();
 	}
 
 	@Override
