d85a91c6e38bab7ccf41c7a04cda30689de1f80b
Grzegorz Grzybek <gr.grzybek@gmail.com>
2022-04-08 09:28:22 +0200

    Fix CS error

--- pax-web-itest/pax-web-itest-osgi/src/test/java/org/ops4j/pax/web/itest/osgi/ManifestIntegrationTest.java
+++ pax-web-itest/pax-web-itest-osgi/src/test/java/org/ops4j/pax/web/itest/osgi/ManifestIntegrationTest.java
@@ -26,7 +26,6 @@ import java.util.jar.Manifest;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
