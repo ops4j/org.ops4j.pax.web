a4b274d8aac4863d605425277b2f2a6e03ebaf3f
Grzegorz Grzybek <gr.grzybek@gmail.com>
2022-09-19 09:15:44 +0200

    [Fixes #1763] Remove Jetty JaspiAuthenticatorFactory from /META-INF/services in pax-web-jetty-bundle

--- pax-web-itest/pax-web-itest-common/src/main/java/org/ops4j/pax/web/itest/AbstractControlledTestBase.java
+++ pax-web-itest/pax-web-itest-common/src/main/java/org/ops4j/pax/web/itest/AbstractControlledTestBase.java
@@ -277,6 +277,19 @@ public abstract class AbstractControlledTestBase {
 		};
 	}
 
+	protected Option[] paxWebJspForTomcatBundle() {
+		return new Option[] {
+				mavenBundle("jakarta.el", "jakarta.el-api")
+						.versionAsInProject().startLevel(START_LEVEL_TEST_BUNDLE - 1),
+				mavenBundle("org.ops4j.pax.web", "pax-web-compatibility-el2")
+						.versionAsInProject().startLevel(START_LEVEL_TEST_BUNDLE - 2).noStart(),
+				mavenBundle("org.eclipse.jdt", "ecj")
+						.versionAsInProject().startLevel(START_LEVEL_TEST_BUNDLE - 1),
+				mavenBundle("org.ops4j.pax.web", "pax-web-jsp")
+						.versionAsInProject().startLevel(START_LEVEL_TEST_BUNDLE - 1)
+		};
+	}
+
 	/**
 	 * Installation of all the bundles required by {@code pax-web-jetty}
 	 * @return
