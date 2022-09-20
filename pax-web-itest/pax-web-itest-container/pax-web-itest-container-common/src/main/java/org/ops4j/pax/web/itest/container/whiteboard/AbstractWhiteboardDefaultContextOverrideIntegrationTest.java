c4efc95ebe1e5ecd4d7763f3f04267f9213a845d
Grzegorz Grzybek <gr.grzybek@gmail.com>
2022-09-20 11:52:33 +0200

    [#1769] Prepare a whiteboard sample + test which overrides default "/" context

---
+++ pax-web-itest/pax-web-itest-container/pax-web-itest-container-common/src/main/java/org/ops4j/pax/web/itest/container/whiteboard/AbstractWhiteboardDefaultContextOverrideIntegrationTest.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.web.itest.container.whiteboard;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.ops4j.pax.web.itest.container.AbstractContainerTestBase;
+import org.ops4j.pax.web.itest.utils.client.HttpTestClientFactory;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+import javax.inject.Inject;
+
+public abstract class AbstractWhiteboardDefaultContextOverrideIntegrationTest extends AbstractContainerTestBase {
+
+	private Bundle bundle;
+
+	@Before
+	public void setUp() throws Exception {
+		configureAndWaitForServletWithMapping("/wb/*",
+				() -> bundle = installAndStartBundle(sampleURI("whiteboard-2")));
+	}
+
+	@After
+	public void tearDown() throws BundleException {
+		if (bundle != null) {
+			bundle.stop();
+			bundle.uninstall();
+		}
+	}
+
+	@Test
+	public void testWhiteboardDynamics() throws Exception {
+		HttpTestClientFactory.createDefaultTestClient()
+				.withResponseAssertion("Response must contain 'Overriden default / context' and 'whiteboard2'",
+						resp -> resp.contains("Overriden default / context") && resp.contains("whiteboard2"))
+				.doGETandExecuteTest("http://127.0.0.1:8181/wb/test");
+	}
+
+}
