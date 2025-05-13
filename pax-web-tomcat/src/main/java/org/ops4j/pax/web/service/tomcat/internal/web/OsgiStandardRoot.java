066e486480c8020bd391e92c778d666001441456
Grzegorz Grzybek <gr.grzybek@gmail.com>
2025-05-13 11:53:50 +0200

    [#2044] Fix code after upgrading to Tomcat 11.0.7
    (cherry picked from commit d48195e251db262ae54490a1ddfe0d55840ac5ce)

--- pax-web-tomcat/src/main/java/org/ops4j/pax/web/service/tomcat/internal/web/OsgiStandardRoot.java
+++ pax-web-tomcat/src/main/java/org/ops4j/pax/web/service/tomcat/internal/web/OsgiStandardRoot.java
@@ -230,6 +230,16 @@ class OsgiStandardRoot extends StandardRoot {
 				public void gc() {
 					// no op
 				}
+
+				@Override
+				public void setAllowLinking(boolean b) {
+					// no op
+				}
+
+				@Override
+				public boolean getAllowLinking() {
+					return false;
+				}
 			};
 		}
 	}
