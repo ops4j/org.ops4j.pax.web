7e7ff88da795f1e098c4809de55da211ce11e1dc
Grzegorz Grzybek <gr.grzybek@gmail.com>
2026-02-27 11:31:34 +0100

    [Fixes #2144] Upgrade to Undertow 2.2.39.Final

--- pax-web-itest/pax-web-itest-container/pax-web-itest-undertow/src/test/java/org/ops4j/pax/web/itest/undertow/war/WarPostIntegrationTest.java
+++ pax-web-itest/pax-web-itest-container/pax-web-itest-undertow/src/test/java/org/ops4j/pax/web/itest/undertow/war/WarPostIntegrationTest.java
@@ -58,7 +58,9 @@ public class WarPostIntegrationTest extends AbstractWarPostIntegrationTest {
 	protected int getPostSizeExceededHttpResponseCode() {
 		// TODO: create an UNDERTOW jira issue. io.undertow.server.RequestTooBigException should be checked
 		//       as the cause of IllegalStateException and better error code should be used (413)
-		return HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+//		return HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+		// https://issues.redhat.com/browse/UNDERTOW-2556 changed from 500 to 400. still not perfect...
+		return HttpServletResponse.SC_BAD_REQUEST;
 	}
 
 }
--- pax-web-itest/pax-web-itest-karaf/src/test/java/org/ops4j/pax/web/itest/karaf/PostLimitUndertowKarafTest.java
+++ pax-web-itest/pax-web-itest-karaf/src/test/java/org/ops4j/pax/web/itest/karaf/PostLimitUndertowKarafTest.java
@@ -53,7 +53,9 @@ public class PostLimitUndertowKarafTest extends PostLimitBaseKarafTest {
 	protected int getPostSizeExceededHttpResponseCode() {
 		// TODO: create an UNDERTOW jira issue. io.undertow.server.RequestTooBigException should be checked
 		//       as the cause of IllegalStateException and better error code should be used (413)
-		return HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+//		return HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+		// https://issues.redhat.com/browse/UNDERTOW-2556 changed from 500 to 400. still not perfect...
+		return HttpServletResponse.SC_BAD_REQUEST;
 	}
 
 }
