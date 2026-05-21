40a8a13a764f0dd7fcb27b4c0d94408eb220c0aa
Holger Friedrich <mail@holger-friedrich.de>
2026-05-21 18:12:07 +0200

    [Fixes #2188] Publish service.id in HttpServiceRuntime's ServiceReferenceDTO properties
    Copy service.id into the properties map alongside the existing
    osgi.http.service.id.
    
    Signed-off-by: Holger Friedrich <mail@holger-friedrich.de>
    (cherry picked from commit c2f35152fdbea9ccbf012ebab02e68e514e158a6)

--- pax-web-runtime/src/main/java/org/ops4j/pax/web/service/internal/Activator.java
+++ pax-web-runtime/src/main/java/org/ops4j/pax/web/service/internal/Activator.java
@@ -606,6 +606,7 @@ public class Activator implements BundleActivator, PaxWebManagedService.Configur
 			httpServiceRuntimeDTO.id = (long) httpServiceRuntimeReg.getReference().getProperty(Constants.SERVICE_ID);
 			httpServiceRuntimeDTO.bundle = bundleContext.getBundle().getBundleId();
 			httpServiceRuntimeDTO.properties = new HashMap<>();
+			httpServiceRuntimeDTO.properties.put(Constants.SERVICE_ID, httpServiceRuntimeDTO.id);
 			httpServiceRuntimeDTO.properties.put(HttpServiceRuntimeConstants.HTTP_SERVICE_ENDPOINT, "/");
 			httpServiceRuntimeDTO.properties.put(HttpServiceRuntimeConstants.HTTP_SERVICE_ID, Collections.singletonList(httpServiceId));
 			httpServiceRuntimeDTO.properties.put("service.changecount", 0L);
